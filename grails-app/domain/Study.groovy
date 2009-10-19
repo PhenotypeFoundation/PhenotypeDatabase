25b9655d5fd225882d276876bd34e23b8f76f19c
varaprasad22 <email2gajula@gmail.com>
2009-10-19 12:33:49 +0000

    Added Keyword expansion

---
+++ grails-app/domain/Study.groovy
@@ -0,0 +1,17 @@
+/**
+ * Study Domain
+ *
+ * @author	email2gajula@gmail.com
+ * @since	20091019
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+
+class Study {
+
+    static constraints = {
+    }
+}
