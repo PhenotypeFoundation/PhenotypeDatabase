1644471021bf1a64efcc4f381e8a58cbf1970e0f
4np <work@osx.eu>
2010-01-05 10:38:58 +0000

    - Adem's previous commit included the missing domain class... uncommented the study inclusions

--- grails-app/controllers/HomeController.groovy
+++ grails-app/controllers/HomeController.groovy
@@ -1,7 +1,21 @@
-//import dbnp.studycapturing.Study
+import dbnp.studycapturing.Study
 
+/**
+ * Home Controler
+ *
+ * My Description
+ *
+ * @author  Kees van Bochove
+ * @since   20091102
+ * @package studycapturing
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
 class HomeController {
     def index = {
-       //[ studyCount: dbnp.studycapturing.Study.count() ]
+       [ studyCount: dbnp.studycapturing.Study.count() ]
     }
 }
--- grails-app/controllers/StudyController.groovy
+++ grails-app/controllers/StudyController.groovy
@@ -1,4 +1,17 @@
+/**
+ * Home Controler
+ *
+ * My Description
+ *
+ * @author  Kees van Bochove
+ * @since   20091028
+ * @package studycapturing
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
 class StudyController extends BaseController {
-
-    //def scaffold = dbnp.studycapturing.Study
+    def scaffold = dbnp.studycapturing.Study
 }
