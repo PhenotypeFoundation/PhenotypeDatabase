67dc1f8ea0b15d3c036b4c8ea175038fec493441
4np <work@osx.eu>
2010-01-25 11:48:07 +0000

    - re-added TemplateSubjectField.groovy which was deleted in revision 126

---
+++ grails-app/domain/dbnp/studycapturing/TemplateSubjectField.groovy
@@ -0,0 +1,13 @@
+package dbnp.studycapturing
+
+/**
+ * Instances of this class describe an extra (template) field for the Subject entity.
+ */
+class TemplateSubjectField extends TemplateField {
+	static constraints = {
+	}
+
+	def String toString() {
+		super.toString()
+	}
+}
