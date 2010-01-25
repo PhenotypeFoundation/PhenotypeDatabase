1168b5d2b836f9024d0073934904df7740b0d36e
jtsaito <jahn.saito@gmail.com>
2010-01-25 07:42:50 +0000

    Committed as part of Event editing.

--- grails-app/domain/dbnp/studycapturing/ProtocolInstance.groovy
+++ grails-app/domain/dbnp/studycapturing/ProtocolInstance.groovy
@@ -7,9 +7,9 @@ class ProtocolInstance {
 
     Protocol protocol
 
-    // TODO: check how the values can be indexed so that they can be mapped to their respective parameters (should we should use maps here?) 
-    static hasMany = [stringParameterValues : String, numberParameterValues : double, listParameterValues: long]
 
+    // TODO: check how the values can be indexed so that they can be mapped to their respective parameters (should we use maps here?) 
+    static hasMany = [values : ProtocolParameterInstance]
     static constraints = {
     }
 }
