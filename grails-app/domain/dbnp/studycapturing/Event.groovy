957656f9ed65d0ed04c064094f5921fe46df8d52
jtsaito <jahn.saito@gmail.com>
2010-01-25 07:40:43 +0000

    Committed as part of Event editing.

--- grails-app/domain/dbnp/studycapturing/Event.groovy
+++ grails-app/domain/dbnp/studycapturing/Event.groovy
@@ -1,5 +1,7 @@
 package dbnp.studycapturing
 
+import groovy.time.*
+
 /**
  * The Event class describes an actual event, as it has happened to a certain subject. Often, the same event occurs
  * to multiple subjects at the same time. That is why the actual description of the event is factored out into a more
@@ -12,10 +14,29 @@ class Event {
     Date startTime
     Date endTime
 
-    static constraints = {
+
+    // static constraints = { }
+    def getDuration() {
+        //endTime - startTime  // this is not documented and does not work either
+	                       // so, it's useless.
+			       // thus, do this manually as follows
+
+        def timeMillis = (endTime.getTime() - startTime.getTime()).abs()
+        def days    = (timeMillis/(1000*60*60*24)).toInteger()
+        def hours   = (timeMillis/(1000*60*60)).toInteger()
+        def minutes = (timeMillis/(1000*60)).toInteger()
+        def seconds = (timeMillis/1000).toInteger()
+        def millis  = (timeMillis%1000).toInteger()
+
+        return new Duration(days,hours,minutes,seconds,millis)
     }
 
-    def getDuration() {
-        endTime - startTime
+
+    def getDurationString()
+    {
+        def d = getDuration()
+	return  "${d.days} days, ${d.hours} hrs, ${d.minutes} min, ${d.seconds} sec."
     }
+
+
 }
