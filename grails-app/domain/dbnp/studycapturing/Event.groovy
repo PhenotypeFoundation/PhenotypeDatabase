497d6ec6b33fa0067c989d6945854536e11a40c4
jtsaito <jahn.saito@gmail.com>
2010-01-25 08:31:34 +0000

    The Term class has been seperately created in dbnp.studycapturing.data and dbnp.studycapturing.data. Since there is no big difference, I retracted the later domain class. This requried adjustment of some imports in the controllers.

--- grails-app/domain/dbnp/studycapturing/Event.groovy
+++ grails-app/domain/dbnp/studycapturing/Event.groovy
@@ -2,6 +2,7 @@ package dbnp.studycapturing
 
 import groovy.time.*
 
+
 /**
  * The Event class describes an actual event, as it has happened to a certain subject. Often, the same event occurs
  * to multiple subjects at the same time. That is why the actual description of the event is factored out into a more
