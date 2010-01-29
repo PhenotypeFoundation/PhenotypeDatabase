7dcb84645de80cad1eb008a960b570d122eb1676
jtsaito <jahn.saito@gmail.com>
2010-01-29 18:38:01 +0000

    Currently Sample extends Event. The Sample class might better be merged into the Event class.

--- grails-app/domain/dbnp/studycapturing/Sample.groovy
+++ grails-app/domain/dbnp/studycapturing/Sample.groovy
@@ -11,12 +11,11 @@ class Sample {
     // TODO: should Sample also carry a reference to its parent study,
     // or should this be inferred via the parent SamplingEvent?
 
-    // TODO: should Sample also carry a reference to its parent subject,
-    // or should this be inferred via the parent SamplingEvent?
-
-    String name // should be unique with respect to the parent study (which can be inferred
+    String name      // should be unique with respect to the parent study (which can be inferred)
     Term material
+    // don't we need a member, that describes the quantity of the sample?
 
     static constraints = {
     }
+
 }
--- grails-app/domain/dbnp/studycapturing/SamplingEvent.groovy
+++ grails-app/domain/dbnp/studycapturing/SamplingEvent.groovy
@@ -2,7 +2,12 @@ package dbnp.studycapturing
 
 /**
  * The SamplingEvent class describes a sampling event, an event that also results in one or more samples.
+ *
+ * NOTE: according to documentation, super classes and subclasses share the same table.
+ *       thus, there is actually no reason not to merge the sampling with the Event super class.
+ *       this is especially true, since the subclass adds only one has-many relation ship.
  */
+
 class SamplingEvent extends Event {
 
     static hasMany = [samples : Sample]
