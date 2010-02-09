92ed02d03f95a3ea24ea5e8a27f73b1909f6d913
keesvanbochove <kees@thehyve.nl>
2010-02-09 10:53:21 +0000

    added missing class for study template

---
+++ grails-app/domain/dbnp/studycapturing/TemplateStudyField.groovy
@@ -0,0 +1,19 @@
+package dbnp.studycapturing
+
+/**
+ * Instances of this class describe an extra (template) field for the Subject entity.
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+
+class TemplateStudyField extends TemplateField {
+	static constraints = {
+	}
+
+	def String toString() {
+		super.toString()
+	}
+}
