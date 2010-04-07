3417672bc42ab1a62e09ad5fdf65663b19995563
tjeerdnet <t.w.abma@gmail.com>
2010-04-07 12:03:10 +0000

    - added Human study to Bootstrap for testing with importer

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -299,6 +299,16 @@ class BootStrap {
 				startDate: Date.parse('yyyy-MM-dd','2007-12-11')
 			).with { if (!validate()) { errors.each { println it} } else save()}
 
+			def exampleHumanStudy = new Study(
+				template: humanTemplate,
+				title:"Human example template",
+				code:"Human example code",
+				researchQuestion:"Leptin etc.",
+				description:"C57Bl/6 mice were fed a high fat (45 en%) or low fat (10 en%) diet after a four week run-in on low fat diet. After 1 week 10 mice that received a low fat diet were given an IP leptin challenge and 10 mice of the low-fat group received placebo injections. The same procedure was performed with mice that were fed the high-fat diet. After 4 weeks the procedure was repeated. In total 80 mice were culled.",
+				ecCode:"2007117.c",
+				startDate: Date.parse('yyyy-MM-dd','2007-12-11')
+			).with { if (!validate()) { errors.each { println it} } else save()}
+
 			def evLF = new Event(
 				startTime: Date.parse('yyyy-MM-dd','2008-01-07'),
 				endTime: Date.parse('yyyy-MM-dd','2008-01-14'),
