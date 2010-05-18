0e38846c940987907aaf8b7e0cd1334ea556a419
keesvanbochove <kees@thehyve.nl>
2010-05-18 06:39:30 +0000

    refactored Ontology tests, added Ontocat example as a test

--- test/integration/gscf/OntologyTests.groovy
+++ test/integration/gscf/OntologyTests.groovy
@@ -3,6 +3,7 @@ package gscf
 import grails.test.*
 import dbnp.data.*
 
+
 /**
  * OntologyTests Test
  *
@@ -30,19 +31,6 @@ class OntologyTests extends GrailsUnitTestCase {
 
 	protected void setUp() {
 		super.setUp()
-	}
-
-	protected void tearDown() {
-		super.tearDown()
-	}
-
-	/**
-	 * Test creation and saving of ontologies and terms
-	 * and giveTerms and giveTermByName methods
-	 */
-	void testAll () {
-
-		// === TEST Ontology
 
 		def ontology = new Ontology(
 				name: testOntologyName,
@@ -56,6 +44,16 @@ class OntologyTests extends GrailsUnitTestCase {
 		// Validate and save ontology
 		assert ontology.validate()
 		assert ontology.save(flush:true)
+	}
+
+	protected void tearDown() {
+		super.tearDown()
+	}
+
+	/**
+	 * Test if ontology was properly saved
+	 */
+	void testSave () {
 
 		// Try to retrieve the ontology and make sure it's the same
 		def ontologyDB = Ontology.findByName(testOntologyName)
@@ -67,10 +65,13 @@ class OntologyTests extends GrailsUnitTestCase {
 		assert ontologyDB.ncboId.equals(testOntologyNcboId)
 		assert ontologyDB.ncboVersionedId.equals(testOntologyNcboVersionedId)
 
-		// Apparently, the saved ontology is not persisted between test methods :-(
-		// Otherwise, we could separate these parts into different tests
+	}
 
-		// === TEST Term and giveTermByName
+
+	/**
+	* Test saving and retrieving a term within the ontology and test giveTermByName(name)
+	*/
+	void testTermSave() {
 
 		// Find created ontology
 		def testOntology = Ontology.findByName(testOntologyName)
@@ -91,13 +92,49 @@ class OntologyTests extends GrailsUnitTestCase {
 		assert termDB.name.equals(testTermName)
 		assert termDB.accession.equals(testAccession)
 		assert termDB.ontology == testOntology
+	}
 
-		// === TEST giveTerms
-		
+
+	/**
+	* Test giveTerms() method
+	*/
+	void testGiveTerms() {
+
+		// Find created ontology
+		def testOntology = Ontology.findByName(testOntologyName)
+		assert testOntology
+
 		def terms = testOntology.giveTerms()
 		assert terms
 		assert terms.size() == 1
 		assert terms.asList().first().name.equals(testTermName)
-
 	}
+
+
+	/**
+ 	* Ontocat test (Ontocat example 1)
+ 	*
+	* Shows how to list all the available ontologies in OLS
+ 	*
+ 	*/
+	void testOntocat() {
+		// Instantiate OLS service
+		uk.ac.ebi.ontocat.OntologyService os = new uk.ac.ebi.ontocat.bioportal.BioportalOntologyService()
+		// For all ontologies in OLS print their
+		// full label and abbreviation
+		uk.ac.ebi.ontocat.Ontology o = os.getOntology("1005")
+			StringBuilder sb = new StringBuilder();
+			sb.append(o.getAbbreviation());
+			sb.append("\t");
+			sb.append(o.getLabel());
+			sb.append("\t");
+			sb.append(o.getOntologyAccession());
+			sb.append("\t");
+			o.getVersionNumber() + o.getMetaPropertyValues().each {
+				sb.append(it.dump())
+			}
+			System.out.println(sb.toString());
+		
+	}
+
 }
