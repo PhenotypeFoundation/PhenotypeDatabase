c339db84ebe488bbd88b32be88ca2067594c7eaf
4np <work@osx.eu>
2010-08-05 22:06:41 +0000

    - nimble is blocking war building... removing nimble altogether for now

--- grails-app/domain/nimble/Profile.groovy
+++ grails-app/domain/nimble/Profile.groovy
@@ -1,16 +1,8 @@
-/**
- * Nimble Profile Domain Class
- *
- * @see http://www.grails.org/plugin/nimble
- *
- * Revision information:
- * $Rev$
- * $Author$
- * $Date$
- */
 
 package nimble
 
-class Profile extends grails.plugins.nimble.core.ProfileBase {
+class Profile { /* extends grails.plugins.nimble.core.ProfileBase {  */
+
 	// Extend ProfileBase with your custom values here
+
 }
--- grails-app/domain/nimble/User.groovy
+++ grails-app/domain/nimble/User.groovy
@@ -1,18 +1,8 @@
-/**
- * Nimble Profile Domain Class
- *
- * @see http://www.grails.org/plugin/nimble
- *
- * Revision information:
- * $Rev$
- * $Author$
- * $Date$
- */
 
 package nimble
 
-class User extends grails.plugins.nimble.core.UserBase {
-	def String toString() {
-		return this.username;
-	}
+class User { /* extends grails.plugins.nimble.core.UserBase { */
+
+	// Extend UserBase with your custom values here
+
 }
