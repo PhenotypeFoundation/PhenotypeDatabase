aa3a6f8936dd147a913ed0fe6c00d54adf521de3
tjeerdnet <t.w.abma@gmail.com>
2010-10-06 08:03:11 +0000

    - Configuration dummy update to trigger rebuild

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,7 +11,7 @@
  */
 
 // locations to search for config files that get merged into the main config
-// config files can either be Java properties files or ConfigSlurper scripts
+// config files can either be Java properties files or ConfigSlurper scripts.
 
 // grails.config.locations = [ "classpath:${appName}-config.properties",
 //                             "classpath:${appName}-config.groovy",
