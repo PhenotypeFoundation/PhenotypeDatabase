e767857f1ce5acd268083f3bd4f155090f38b70b
4np <work@osx.eu>
2010-11-09 17:53:25 +0000

    - bootstrapping tried to insert duplicate ontologies...

--- grails-app/conf/BootStrapTemplates.groovy
+++ grails-app/conf/BootStrapTemplates.groovy
@@ -19,49 +19,10 @@ class BootStrapTemplates {
 	 * (e.g. in development or automated test environments)
 	 */
 	public static void initTemplateOntologies() {
-
-		// add Subject>species ontology
-		println ".adding NCBI species ontology"
-		def speciesOntology = new Ontology(
-			name: 'NCBI organismal classification',
-			description: 'A taxonomic classification of living organisms and associated artifacts for their controlled description within the context of databases.',
-			url: 'http://www.ncbi.nlm.nih.gov/Taxonomy/taxonomyhome.html/',
-			versionNumber: '1.2',
-			ncboId: '1132',
-			ncboVersionedId: '38802'
-		).with { if (!validate()) { errors.each { println it} } else save()}
-
-		// add Sample>material ontology
-		println ".adding BRENDA source material ontology"
-		def brendaOntology = new Ontology(
-			name: 'BRENDA tissue / enzyme source',
-			description: 'A structured controlled vocabulary for the source of an enzyme. It comprises terms for tissues, cell lines, cell types and cell cultures from uni- and multicellular organisms.',
-			url: 'http://www.brenda-enzymes.info',
-			versionNumber: '1.3',
-			ncboId: '1005',
-			ncboVersionedId: '40643'
-		).with { if (!validate()) { errors.each { println it} } else save()}
-
-		// add NCI ontology which is used in Mouse genotype template field
-		def nciOntology = new Ontology(
-			name: 'NCI Thesaurus',
-			description: 'A vocabulary for clinical care, translational and basic research, and public information and administrative activities.',
-			url: 'http://ncicb.nci.nih.gov/core/EVS',
-			versionNumber: '10.01',
-			ncboId: '1032',
-			ncboVersionedId: '42693'
-		).with { if (!validate()) { errors.each { println it} } else save()}
-
-		// add CHEBI ontology which is used in Mouse genotype template field
-		def chebiOntology = new Ontology(
-			name: 'Chemical entities of biological interest',
-			description: 'A structured classification of chemical compounds of biological relevance.',
-			url: 'http://www.ebi.ac.uk/chebi',
-			versionNumber: '1.68',
-			ncboId: '1007',
-			ncboVersionedId: '42878'
-		).with { if (!validate()) { errors.each { println it} } else save()}
-		
+		def speciesOntology = Ontology.getOrCreateOntologyByNcboId(1132)
+		def brendaOntology = Ontology.getOrCreateOntologyByNcboId(1005)
+		def nciOntology = Ontology.getOrCreateOntologyByNcboId(1032)
+		def chebiOntology = Ontology.getOrCreateOntologyByNcboId(1007)		
 	}
 
 	/**
