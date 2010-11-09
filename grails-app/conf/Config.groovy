ea5c966b4b50c1901adfd042eb3bb639af91f360
4np <work@osx.eu>
2010-11-09 13:55:29 +0000

    - added environments used by build scripts (e.g. ci, test and www)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,8 +1,8 @@
 /**
  * Application Configuration
  *
- * @author  Jeroen Wesbeek
- * @since	20100520
+ * @author Jeroen Wesbeek
+ * @since 20100520
  *
  * Revision information:
  * $Rev$
@@ -23,79 +23,87 @@
 // }
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
-grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
-                      xml: ['text/xml', 'application/xml'],
-                      text: 'text/plain',
-                      js: 'text/javascript',
-                      rss: 'application/rss+xml',
-                      atom: 'application/atom+xml',
-                      css: 'text/css',
-                      csv: 'text/csv',
-                      all: '*/*',
-                      json: ['application/json','text/json'],
-                      form: 'application/x-www-form-urlencoded',
-                      multipartForm: 'multipart/form-data'
-                    ]
+grails.mime.types = [html: ['text/html', 'application/xhtml+xml'],
+	xml: ['text/xml', 'application/xml'],
+	text: 'text/plain',
+	js: 'text/javascript',
+	rss: 'application/rss+xml',
+	atom: 'application/atom+xml',
+	css: 'text/css',
+	csv: 'text/csv',
+	all: '*/*',
+	json: ['application/json', 'text/json'],
+	form: 'application/x-www-form-urlencoded',
+	multipartForm: 'multipart/form-data'
+]
 // The default codec used to encode data with ${}
-grails.views.default.codec="none" // none, html, base64
-grails.views.gsp.encoding="UTF-8"
-grails.converters.encoding="UTF-8"
+grails.views.default.codec = "none" // none, html, base64
+grails.views.gsp.encoding = "UTF-8"
+grails.converters.encoding = "UTF-8"
 
 // enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = true
 
 // set per-environment serverURL stem for creating absolute links
 environments {
-    production {
-        grails.serverURL = "http://gscf.nmcdsp.org"
-    }
-    development {
-        grails.serverURL = "http://localhost:8080/${appName}"
-    }
-    test {
-        grails.serverURL = "http://localhost:8080/${appName}"
-    }
-
+	development {
+		grails.serverURL = "http://localhost:8080/${appName}"
+	}
+	ci {
+		// used by build script
+		grails.serverURL = "http://ci.gscf.nmcdsp.org"
+	}
+	test {
+		// used by build script
+		grails.serverURL = "http://test.gscf.nmcdsp.org"
+	}
+	production {
+		grails.serverURL = "http://www.nmcdsp.org"
+	}
+	www {
+		// used by build script
+		grails.serverURL = "http://www.nmcdsp.org"
+	}
 }
 
 // log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+	// Example of changing the log pattern for the default console
+	// appender:
+	//
+	//appenders {
+	//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+	//}
 
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-	       'org.codehaus.groovy.grails.web.pages', //  GSP
-	       'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-	       'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-	       'org.codehaus.groovy.grails.web.mapping', // URL mapping
-	       'org.codehaus.groovy.grails.commons', // core / classloading
-	       'org.codehaus.groovy.grails.plugins', // plugins
-	       'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-	       'org.springframework',
-	       'org.hibernate'
+	error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
+		'org.codehaus.groovy.grails.web.pages', //  GSP
+		'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+		'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+		'org.codehaus.groovy.grails.web.mapping', // URL mapping
+		'org.codehaus.groovy.grails.commons', // core / classloading
+		'org.codehaus.groovy.grails.plugins', // plugins
+		'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+		'org.springframework',
+		'org.hibernate'
 
-    warn   'org.mortbay.log'
+	warn 'org.mortbay.log'
 
-    //debug 'org.codehaus.groovy.grails.plugins'
+	//debug 'org.codehaus.groovy.grails.plugins'
 }
 
 graphviz {
-    // graphviz installation path is dependent on OS
-    // (requirement for class diagram plugin)
-        switch (System.properties["os.name"]) {
-            case "Mac OS X":
-                // define mac path to Graphviz dot executable
-                // (install using macports: sudo port install graphviz)
-                dot.executable = "/opt/local/bin/dot"
-                break
-            default:
-                // assume the linux default path
-                dot.executable = "/usr/bin/dot"
-        }
+	// graphviz installation path is dependent on OS
+	// (requirement for class diagram plugin)
+	switch (System.properties["os.name"]) {
+		case "Mac OS X":
+			// define mac path to Graphviz dot executable
+			// (install using macports: sudo port install graphviz)
+			dot.executable = "/opt/local/bin/dot"
+			break
+		default:
+			// assume the linux default path
+			dot.executable = "/usr/bin/dot"
+	}
 }
 
 // cryptography settings
@@ -143,19 +151,19 @@ gscf {
 		// importable entities
 		// use: grailsApplication.config.gscf.domain.importableEntities
 		// @see ImporterController
-		importableEntities: [			
-			event	: [name: 'Event', entity:'dbnp.studycapturing.Event'],			
-			sample	: [name: 'Sample', entity: 'dbnp.studycapturing.Sample'],
-			study	: [name: 'Study', entity: 'dbnp.studycapturing.Study'],
-			subject	: [name: 'Subject', entity: 'dbnp.studycapturing.Subject'],
-			samplingevent : [name: 'SamplingEvent', entity: 'dbnp.studycapturing.SamplingEvent']
+		importableEntities: [
+			event: [name: 'Event', entity: 'dbnp.studycapturing.Event'],
+			sample: [name: 'Sample', entity: 'dbnp.studycapturing.Sample'],
+			study: [name: 'Study', entity: 'dbnp.studycapturing.Study'],
+			subject: [name: 'Subject', entity: 'dbnp.studycapturing.Subject'],
+			samplingevent: [name: 'SamplingEvent', entity: 'dbnp.studycapturing.SamplingEvent']
 
 		]
 	]
 }
 
 // jquery plugin
-grails.views.javascript.library="jquery"
+grails.views.javascript.library = "jquery"
 
 // Needed for the Spring Security Core plugin:
 grails.plugins.springsecurity.userLookup.userDomainClassName = 'dbnp.authentication.SecUser'
@@ -174,45 +182,30 @@ grails.plugins.springsecurity.ui.forgotPassword.emailSubject = 'Password reset G
 // Make sure the different controllers provided by springsecurity.ui are only accessible by administrators
 // NB: the RegisterController is used for forgotten passwords. It should be accessible by anyone
 grails.plugins.springsecurity.controllerAnnotations.staticRules = [
-    '/user/**': ['ROLE_ADMIN'],
-    '/role/**': ['ROLE_ADMIN'],
-    '/aclclass/**': ['ROLE_ADMIN'],
-    '/aclentry/**': ['ROLE_ADMIN'],
-    '/aclobjectidentity/**': ['ROLE_ADMIN'],
-    '/aclsid/**': ['ROLE_ADMIN'],
-    '/persistentlogin/**': ['ROLE_ADMIN'],
-    '/registrationcode/**': ['ROLE_ADMIN'],
-    '/requestmap/**': ['ROLE_ADMIN'],
-    '/securityinfo/**': ['ROLE_ADMIN']
+	'/user/**': ['ROLE_ADMIN'],
+	'/role/**': ['ROLE_ADMIN'],
+	'/aclclass/**': ['ROLE_ADMIN'],
+	'/aclentry/**': ['ROLE_ADMIN'],
+	'/aclobjectidentity/**': ['ROLE_ADMIN'],
+	'/aclsid/**': ['ROLE_ADMIN'],
+	'/persistentlogin/**': ['ROLE_ADMIN'],
+	'/registrationcode/**': ['ROLE_ADMIN'],
+	'/requestmap/**': ['ROLE_ADMIN'],
+	'/securityinfo/**': ['ROLE_ADMIN']
 ]
-
 
 // Needed for sending emails
 grails {
-    mail {
-        host = "smtp.gmail.com"
-        port = 465
-        username = "gscfproject@gmail.com"
-        password = "gscf2010"
-        props = [
-            "mail.smtp.auth":"true",
-            "mail.smtp.socketFactory.port": '465',
-            "mail.smtp.socketFactory.class": "javax.net.ssl.SSLSocketFactory",
-            "mail.smtp.socketFactory.fallback": "false"
-        ]
-    }
-}
-
-// The OAuth consumer key and secret variables are currently replaced by Jeroen's Continuous Integration script.
-// These keys were requested by me (Tjeerd) from myExperiment.
-oauth {
-	myExperiment {
-		requestTokenUrl = 'http://www.myexperiment.org/oauth/request_token'
-		accessTokenUrl = 'http://www.myexperiment.org/oauth/access_token'
-		authUrl = 'http://www.myexperiment.org/oauth/authorize'
-                scope = 'http://example.com/oauth/feed/api/' // Optional - currently required by the Google GData APIs
-		consumer.key = '$oauthkey$'
-		consumer.secret = '$oauthsecret$'
+	mail {
+		host = "smtp.gmail.com"
+		port = 465
+		username = "gscfproject@gmail.com"
+		password = "gscf2010"
+		props = [
+			"mail.smtp.auth": "true",
+			"mail.smtp.socketFactory.port": '465',
+			"mail.smtp.socketFactory.class": "javax.net.ssl.SSLSocketFactory",
+			"mail.smtp.socketFactory.fallback": "false"
+		]
 	}
 }
-
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,73 +13,64 @@ hibernate {
 environments {
 	development {
 		dataSource {
-			//println "DATASOURCE DEBUG :: user.home = "+System.properties["user.home"]
-			switch (System.properties["user.home"]) {
-				case "/Users/adem/TURNEDOFFBYDEFAULT":
-					// Development Postgres Database is turned off by default
-					// if you do want to keep your data you can:
-					// 	- reformate 'case' to your user.home (/Users/adem ?)
-					//	- define development data in the BootStrap.groovy instead
-					dbCreate = "update"
-					username = "gscf"
-					password = "dbnp"
-
-					// PostgreSQL
-					driverClassName = "org.postgresql.Driver"
-					url = "jdbc:postgresql://localhost:5432/gscf"
-					dialect = org.hibernate.dialect.PostgreSQLDialect
-					break;
-				default:
-					// by default we use an in memory development database
-					dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-					url = "jdbc:hsqldb:mem:devDB"
-					//loggingSql = true
-			   		break;
-			}
+			// by default we use an in memory development database
+			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			url = "jdbc:hsqldb:mem:devDB"
+			//loggingSql = true
 		}
 	}
-	test {
+	ci {
+		// used by build script
 		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:mem:testDb"
-		}
-	}
-	production {
-		dataSource {
-			/*
-			 * when releasing a new stable to the live environment
-			 * you would probably comment out the dbCreate option
-			 * so hibernate won't try to update (which is does not
-			 * do so well) and you update the live database yourself
-			 *
-			 * @see http://grails.org/plugin/autobase
-			 * @see http://wiki.github.com/RobertFischer/autobase/example-usage
-			 */
 			dbCreate = "update"
 			username = "gscf"
 			password = "dbnp"
 
 			// PostgreSQL
 			driverClassName = "org.postgresql.Driver"
-			url = "jdbc:postgresql://localhost:5432/gscf"
+			url = "jdbc:postgresql://localhost:5432/gscf-ci"
 			dialect = org.hibernate.dialect.PostgreSQLDialect
 			//logSql = true	// enable logging while not yet final
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			username = "gscf"
+			password = "dbnp"
 
-			/* Apparently Hibernate performs two queries on inserting, one before
-			 * to generate the unique id, and then the insert itself. In PostgreSQL
-			 * > 8.2 this behaviour has changed, however hibernate has not implemented
-			 * this change. In case we might optimize in the future, more info is here:
-			 * http://blog.wolfman.com/articles/2009/11/11/using-postgresql-with-grails
-			 * - Jeroen
-			 */
+			// PostgreSQL
+			driverClassName = "org.postgresql.Driver"
+			url = "jdbc:postgresql://localhost:5432/gscf-test"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			//logSql = true	// enable logging while not yet final
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			username = "gscf"
+			password = "dbnp"
 
-			// MySQL
-			//driverClassName = "com.mysql.jdbc.Driver"
-			//url = "jdbc:mysql://localhost/gscf"
-			//dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+			// PostgreSQL
+			driverClassName = "org.postgresql.Driver"
+			url = "jdbc:postgresql://localhost:5432/gscf-www"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			//logSql = true	// enable logging while not yet final
+		}
+	}
+	www {
+		// used by build script
+		dataSource {
+			dbCreate = "update"
+			username = "gscf"
+			password = "dbnp"
 
-			//In memory
-			//url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+			// PostgreSQL
+			driverClassName = "org.postgresql.Driver"
+			url = "jdbc:postgresql://localhost:5432/gscf-www"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			//logSql = true	// enable logging while not yet final
 		}
 	}
 }
