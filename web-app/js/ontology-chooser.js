b113f52d086af48e0a9a62397005273265330f0c
4np <work@osx.eu>
2010-11-22 18:07:34 +0000

    - extended the ontology chooser with copy / paste support. When you have selected a term from an ontology you can now copy it, and paste it in other ontology fields that accept the same ontology (ncboId) or all ontologies. Partly fixes issue #178

--- web-app/js/ontology-chooser.js
+++ web-app/js/ontology-chooser.js
@@ -23,12 +23,12 @@
  * no ugly HTML tags, you also have to include jquery.ui.autocomplete.html.js
  * in your page!
  *
- * @author      Jeroen Wesbeek
- * @since       20100312
- * @package     wizard
- * @requires    jquery, jquery-ui
- * @see         http://bioportal.bioontology.org/ontologies/
- * @see         http://bioportal.bioontology.org/search/json_search/?q=musculus
+ * @author		Jeroen Wesbeek
+ * @since		20100312
+ * @package		wizard
+ * @requires	jquery, jquery-ui
+ * @see			http://bioportal.bioontology.org/ontologies/
+ * @see			http://bioportal.bioontology.org/search/json_search/?q=musculus
  *
  * Revision information:
  * $Rev$
@@ -38,210 +38,305 @@
 function OntologyChooser() {
 }
 OntologyChooser.prototype = {
-    cache   : [],           // ontology cache
-    options : {
-        minLength   : 3,    // minimum input length before launching Ajax request
-        showHide    : null, // show / hide this DOM element on select/deselect autocomplete results
-        spinner     : '../images/spinner.gif'
-    },
+	cache		: [],		// ontology cache
+	ctrl		: false,	// work variable to search for copy/paste events
+	noSearch	: false,	// work variable to disable/enable autocomplete search
+	clipboard   : [],		// a clipboard to contain copied ontologies
+	options		: {
+		minLength	: 3,	// minimum input length before launching Ajax request
+		showHide	: null,	// show / hide this DOM element on select/deselect autocomplete results
+		spinner		: '../images/spinner.gif'
+	},
 
-    /**
-     * initialize object
-     */
-    init: function(options) {
-        var that = this;
+	/**
+	 * initialize object
+	 */
+	init: function(options) {
+		var that = this;
 
-        // set class parameters
-        if (options) {
-            $.each(options, function(key,value) {
-                that.options[key] = value;
-            });
-        }
+		// set class parameters
+		if (options) {
+			$.each(options, function(key, value) {
+				that.options[key] = value;
+			});
+		}
 
-        // hide showHide div?
-        if (this.options.showHide) {
-            this.options.showHide.hide();
-        }
+		// hide showHide div?
+		if (this.options.showHide) {
+			this.options.showHide.hide();
+		}
 
-        // find all ontology elements
-        $("input[rel*='ontology']").each(function() {
-            that.initAutocomplete(this);
-        });
-    },
+		// find all ontology elements
+		$("input[rel*='ontology']").each(function() {
+			that.initAutocomplete(this);
+		});
+	},
 
-    /**
-     * initialize the ontology autocomplete
-     * @param element
-     */
-    initAutocomplete: function(element) {
-        var that = this
-        var inputElement = $(element);
-        var selected = false;
+	/**
+	 * initialize the ontology autocomplete
+	 * @param element
+	 */
+	initAutocomplete: function(element) {
+		var that = this
+		var inputElement = $(element);
+		var selected = false;
 
-        // determine what ontology to use
-        var values = inputElement.attr('rel').split("-");
-        var ontology_id = values[1];
-        var target_property = values[2];
-        if (ontology_id == "all") { ontology_id = ""; }
+		// determine what ontology to use
+		var values = inputElement.attr('rel').split("-");
+		var ontology_id = values[1];
+		var target_property = values[2];
+		if (ontology_id == "all") {
+			ontology_id = "";
+		}
 
-        // handle keypresses
-        inputElement.bind('keypress', function(e) {
-            // ignore ENTER key in inputElement so the form cannot
-            // be submitted by pressing the ENTER key
-            if (e.keyCode == 13) return false;
+		// handle ctrl or apple keys (to find ctrl-escape / ctrl-paste)
+		inputElement.bind('keydown', function(e) {
+			// check if ctrl-key or apple cmd key is pressed
+			// to start capturing copy events
+			if (e.keyCode == 17 || e.keyCode == 224) that.ctrl = true;
 
-            // when a user uses the backspace the showHide element
-            // (normally the button) gets hidden
-            if (e.keyCode == 8 && that.options.showHide) that.options.showHide.hide();
-        });
+			// ignore ENTER key in inputElement so the form cannot
+			// be submitted by pressing the ENTER key
+			if (e.keyCode == 13) return false;
 
-        // http://bioportal.bioontology.org/search/json_search/?q=musculus
-        inputElement.autocomplete({
-            minLength: that.options.minLength,
-            delay: 300,
-            search: function(event, ui) {
-                if (that.options.spinner) {
-                    inputElement.css({ 'background': 'url(' + that.options.spinner + ') no-repeat right top' });
-                }
-                selected = false;
-            },
-            source: function(request, response) {
-                var q = $.trim(request.term);
-                var url = "http://bioportal.bioontology.org/search/json_search/"+ontology_id+"?q=" + request.term + "&response=json&callback=?";
+			// when a user uses the backspace the showHide element
+			// (normally the button) gets hidden
+			if (e.keyCode == 8 && that.options.showHide) that.options.showHide.hide();
 
-                // got cache?
-                if (that.cache[ q ]) {
-                    // hide spinner
-                    inputElement.css({ 'background': 'none' });
+			// check for 'copy' event
+			if (e.keyCode == 67 && that.ctrl) return that.copy(inputElement);
 
-                    // yeah, lucky us! ;-P
-                    response(that.cache[ q ]);
-                } else {
-                    // nope, fetch it from NCBO
-                    $.getJSON(url, function(data) {
-                        // parse result data
-                        var result = that.parseData(data.data, ontology_id);
+			// check for 'paste' event
+			if (e.keyCode == 86 && that.ctrl) return that.paste(inputElement);
+		});
 
-                        // cache results
-                        that.cache[ q ] = result;
+		// check if ctrl-key is released
+		inputElement.bind('keyup', function(e) {
+			// check if ctrl-key or apple cmd key is released
+			// to start capturing paste events
+			if (e.keyCode == 17 || e.keyCode == 224) that.ctrl = false;
+		});
 
-                        // hide spinner
-                        inputElement.css({ 'background': 'none' });
+		// http://bioportal.bioontology.org/search/json_search/?q=musculus
+		inputElement.autocomplete({
+			minLength: that.options.minLength,
+			delay: 300,
+			search: function(event, ui) {
+				// check if we need to skip searching, generally
+				// after a paste event
+				if (that.noSearch) {
+					// yeah, skip search and reset value
+					that.noSearch = false;
+					return false;
+				}
 
-                        // hide button?
-                        if (!data.data && that.options.showHide) {
-                            that.options.showHide.hide();    
-                        }
+				// set the spinner
+				if (that.options.spinner) {
+					inputElement.css({ 'background': 'url(' + that.options.spinner + ') no-repeat right top' });
+				}
+				selected = false;
+			},
+			source: function(request, response) {
+				var q = $.trim(request.term);
+				var url = "http://bioportal.bioontology.org/search/json_search/" + ontology_id + "?q=" + request.term + "&response=json&callback=?";
+				
+				// got cache?
+				if (that.cache[ q ]) {
+					// hide spinner
+					inputElement.css({ 'background': 'none' });
 
-                        // response callback
-                        response(result);
-                    });
-                }
-            },
-            select: function(event, ui) {
-                // mark that the user selected a suggestion
-                selected = true;
+					// yeah, lucky us! ;-P
+					response(that.cache[ q ]);
+				} else {
+					// nope, fetch it from NCBO
+					$.getJSON(url, function(data) {
+						// parse result data
+						var result = that.parseData(data.data, ontology_id);
 
-                // option selected, set hidden fields
-                var element = inputElement;
+						// cache results
+						that.cache[ q ] = result;
 
-                // set hidden fields
-                that.setInputValue(element, 'concept_id', ui.item.concept_id);
-                that.setInputValue(element, 'ontology_id', ui.item.ontology_id);
-                that.setInputValue(element, 'ncbo_id', ui.item.ncbo_id);
-                that.setInputValue(element, 'full_id', ui.item.full_id);
+						// hide spinner
+						inputElement.css({ 'background': 'none' });
 
-                // remove error class (if present)
-                element.removeClass('error');
+						// no results?
+						if (!data.data) {
+							// hide showHide element?
+							if (that.options.showHide) that.options.showHide.hide();
 
-                // show showHide element if set
-                if (that.options.showHide) {
-                    that.options.showHide.show();
-                }
-            },
-            close: function(event, ui) {
-                // check if the user picked something from the ontology suggestions
-                if (!selected) {
-                    // no he didn't, clear the field(s)
-                    var element = inputElement;
+							// clear hidden fields
+							that.setInputValue(inputElement, 'concept_id', null);
+							that.setInputValue(inputElement, 'ontology_id', null);
+							that.setInputValue(inputElement, 'ncbo_id', null);
+							that.setInputValue(inputElement, 'full_id', null);							
+						}
 
-                    // set fields
-                    inputElement.val('');
-                    that.setInputValue(element, 'concept_id', '');
-                    that.setInputValue(element, 'ontology_id', '');
-                    that.setInputValue(element, 'ncbo_id', '');
-                    that.setInputValue(element, 'full_id', '');
+						// response callback
+						response(result);
+					});
+				}
+			},
+			select: function(event, ui) {
+				// mark that the user selected a suggestion
+				selected = true;
 
-                    // add error class
-                    element.addClass('error');
-                }
-            },
-            html: true
-        });
-    },
+				// option selected, set hidden fields
+				var element = inputElement;
 
-    /**
-     * Set the value of a particular DOM element
-     * @param inputElement
-     * @param name
-     * @param value
-     */
-    setInputValue: function(inputElement, name, value) {
-        var elementName = inputElement.attr('name') + '-' + name;
-        var searchElement = inputElement.parent().find("input[name='" + elementName + "']");
+				// set hidden fields
+				that.setInputValue(element, 'concept_id', ui.item.concept_id);
+				that.setInputValue(element, 'ontology_id', ui.item.ontology_id);
+				that.setInputValue(element, 'ncbo_id', ui.item.ncbo_id);
+				that.setInputValue(element, 'full_id', ui.item.full_id);
 
-        // got a text/hidden field in the DOM?
-        if (searchElement.size() > 0) {
-            // yeah, set it
-            $(searchElement[0]).val(value);
-        } else {
-            // no, dynamically insert it after the input element
-            inputElement.after('<input type="hidden" name="' + elementName + '" value="' + value + '"/>');
-        }
-    },
+				// remove error class (if present)
+				element.removeClass('error');
 
-    /**
-     * Parse the result data
-     *
-     * Data is in the following format:
-     * Mus musculus musculus|birnlex_161|preferred name|29684|http://bioontology.org/projects/ontologies/birnlex#birnlex_161|Mus musculus musculus|Mus musculus musculus|BIRNLex~!~
-     *
-     * Where | codes for a column break, and ~!~ for
-     * a line break
-     *
-     * @param data
-     * @return array
-     */
-    parseData: function(data, ontology_ids) {
-        var parsed = [];
-        var rows = data.split('~!~');
+				// show showHide element if set
+				if (that.options.showHide) {
+					that.options.showHide.show();
+				}
+			},
+			close: function(event, ui) {
+				// check if the user picked something from the ontology suggestions
+				if (!selected) {
+					// no he didn't, clear the field(s)
+					var element = inputElement;
 
-        for (var i=0; i<rows.length; i++) {
-            var row = $.trim(rows[i]);
-            if (row) {
-                var cols = row.split('|');
+					// set fields
+					inputElement.val('');
+					that.setInputValue(element, 'concept_id', '');
+					that.setInputValue(element, 'ontology_id', '');
+					that.setInputValue(element, 'ncbo_id', '');
+					that.setInputValue(element, 'full_id', '');
+
+					// add error class
+					element.addClass('error');
+				}
+			},
+			html: true
+		});
+	},
+
+	/**
+	 * Set the value of a particular DOM element
+	 * @param inputElement
+	 * @param name
+	 * @param value
+	 */
+	setInputValue: function(inputElement, name, value) {
+		var elementName = inputElement.attr('name') + '-' + name;
+		var searchElement = inputElement.parent().find("input[name='" + elementName + "']");
+
+		// got a text/hidden field in the DOM?
+		if (searchElement.size() > 0) {
+			// yeah, set it
+			$(searchElement[0]).val(value);
+		} else {
+			// no, dynamically insert it after the input element
+			inputElement.after('<input type="hidden" name="' + elementName + '" value="' + value + '"/>');
+		}
+	},
+
+	/**
+	 * Get the value of a particular DOM element
+	 * @param inputElement
+	 * @param name
+	 */
+	getInputValue: function(inputElement, name) {
+		var elementName = inputElement.attr('name') + '-' + name;
+		var searchElement = inputElement.parent().find("input[name='" + elementName + "']");
+
+		// got a text/hidden field in the DOM?
+		return (searchElement.size() > 0) ? $(searchElement[0]).val() : '';
+	},
+
+	/**
+	 * Parse the result data
+	 *
+	 * Data is in the following format:
+	 * Mus musculus musculus|birnlex_161|preferred name|29684|http://bioontology.org/projects/ontologies/birnlex#birnlex_161|Mus musculus musculus|Mus musculus musculus|BIRNLex~!~
+	 *
+	 * Where | codes for a column break, and ~!~ for
+	 * a line break
+	 *
+	 * @param data
+	 * @return array
+	 */
+	parseData: function(data, ontology_ids) {
+		var parsed = [];
+		var rows = data.split('~!~');
+
+		for (var i = 0; i < rows.length; i++) {
+			var row = $.trim(rows[i]);
+			if (row) {
+				var cols = row.split('|');
 
 				// If we search in a single ontology, the json doesn't return the
 				// NCBO id in the 8th column (probably because we already know the NCBO id)
 				var ncbo_id;
-				if( cols.length > 8 ) {
+				if (cols.length > 8) {
 					ncbo_id = cols[8];
 				} else {
 					ncbo_id = ontology_ids;
 				}
 
-                parsed[ parsed.length ] = {
-                    value           : cols[0],
-                    label           : cols[0] + ' <span class="about">(' + cols[2] + ')</span> <span class="from">from: ' + cols[ (cols.length-2) ] + '</span>',
-                    preferred_name  : cols[0],  // e.g. Mus musculus musculus
-                    concept_id      : cols[1],  // e.g. birnlex_161
-                    ontology_id     : cols[3],  // e.g. 29684
-                    full_id         : cols[4],  // e.g. http://bioontology.org/projects/ontologies/birnlex#birnlex_161
-                    ncbo_id         : ncbo_id   // e.g. 1494
-                }
-            }
-        }
+				parsed[ parsed.length ] = {
+					value			: cols[0],
+					label			: cols[0] + ' <span class="about">(' + cols[2] + ')</span> <span class="from">from: ' + cols[ (cols.length - 2) ] + '</span>',
+					preferred_name	: cols[0],  // e.g. Mus musculus musculus
+					concept_id		: cols[1],  // e.g. birnlex_161
+					ontology_id		: cols[3],  // e.g. 29684
+					full_id			: cols[4],  // e.g. http://bioontology.org/projects/ontologies/birnlex#birnlex_161
+					ncbo_id			: ncbo_id   // e.g. 1494
+				}
+			}
+		}
 
-        return parsed;
-    }
+		return parsed;
+	},
+
+	/**
+	 * an ontology field is being copied, store all copied data
+	 * in the clipboard
+	 * @param inputElement source input element
+	 */
+	copy: function(inputElement) {
+		this.clipboard = {
+			sourceValue		: $(inputElement).val(),
+			concept_id		: this.getInputValue(inputElement, 'concept_id'),
+			ontology_id		: this.getInputValue(inputElement, 'ontology_id'),
+			ncbo_id			: this.getInputValue(inputElement, 'ncbo_id'),
+			full_id			: this.getInputValue(inputElement, 'full_id')
+		}
+		return false;
+	},
+
+	/**
+	 * paste an copied ontology (if present)
+	 * @param inputElement target input element
+	 */
+	paste: function(inputElement) {
+		// can we paste an ontology?
+		if (this.clipboard.sourceValue &&
+			this.clipboard.concept_id &&
+			this.clipboard.ncbo_id &&
+			this.clipboard.full_id) {
+
+			// check if this target ontology field can accept this copied ontology
+			var pattern = new RegExp(this.clipboard.ncbo_id);
+			if (inputElement.attr('rel').match(pattern) || inputElement.attr('rel').match(/all/)) {
+				// yes, disable search
+				this.noSearch = true;
+
+				// paste values
+				$(inputElement).val(this.clipboard.sourceValue);
+				this.setInputValue(inputElement, 'concept_id', this.clipboard.concept_id);
+				this.setInputValue(inputElement, 'ontology_id', this.clipboard.ontology_id);
+				this.setInputValue(inputElement, 'ncbo_id', this.clipboard.ncbo_id);
+				this.setInputValue(inputElement, 'full_id', this.clipboard.full_id);
+			}
+		}
+		return false;
+	}
 }
