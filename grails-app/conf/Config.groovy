9958333966f969a366eb4f02e5d20667c60a4d92
4np <work@osx.eu>
2011-01-07 12:46:37 +0000

    - disabled loglevel info

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -149,7 +149,7 @@ log4j = {
 	//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
 	//}
 	
-	info "grails.app"
+	// info "grails.app"
 
 	error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
 		'org.codehaus.groovy.grails.web.pages', //  GSP
