23a3dae5ca19e0426a7d4c2527b15d53c68d134e
4np <work@osx.eu>
2011-01-11 17:26:47 +0000

    - resolves issue #269, performing database upgrade if required (change study::description to text)

--- grails-app/conf/DatabaseUpgrade.groovy
+++ grails-app/conf/DatabaseUpgrade.groovy
@@ -1,5 +1,6 @@
 import groovy.sql.Sql
 import dbnp.studycapturing.Study
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 /**
  * A script to automatically perform database changes
@@ -22,15 +23,20 @@ class DatabaseUpgrade {
 		// get a sql instance
 		groovy.sql.Sql sql = new groovy.sql.Sql(dataSource)
 
+		// get configuration
+		def config = ConfigurationHolder.config
+		def db = config.dataSource.driverClassName
+
 		// execute per-change check and upgrade code
-		changeStudyDescription(sql)			// r1245 / r1246
+		changeStudyDescription(sql, db)			// r1245 / r1246
+		changeStudyDescriptionToText(sql, db)	// r1327
 	}
 
 	/**
 	 * execute database change r1245 / r1246 if required
 	 * @param sql
 	 */
-	public static void changeStudyDescription(sql) {
+	public static void changeStudyDescription(sql, db) {
 		// check if we need to perform this upgrade
 		if (sql.firstRow("SELECT count(*) as total FROM template_field WHERE templatefieldentity='dbnp.studycapturing.Study' AND templatefieldname='Description'").total > 0) {
 			// grom that we are performing the upgrade
@@ -68,6 +74,29 @@ class DatabaseUpgrade {
 				sql.execute("DELETE FROM template_field WHERE id=${id}")
 			} catch (Exception e) {
 				"changeStudyDescription database upgrade failed: " + e.getMessage()
+			}
+		}
+	}
+
+	/**
+	 * execute database change r1327 if required
+	 * @param sql
+	 */
+	public static void changeStudyDescriptionToText(sql, db) {
+		// are we running postgreSQL ?
+		if (db == "org.postgresql.Driver") {
+			// check if column 'description' in table 'study' is not of type 'text'
+			if (sql.firstRow("SELECT count(*) as total FROM information_schema.columns WHERE columns.table_schema::text = 'public'::text AND columns.table_name='study' AND column_name='description' AND data_type != 'text'").total > 0) {
+				// grom that we are performing the upgrade
+				"performing database upgrade: study description to text".grom()
+
+				// database upgrade required
+				try {
+					// change the datatype of study::description to text
+					sql.execute("ALTER TABLE study ALTER COLUMN description TYPE text")
+				} catch (Exception e) {
+					"changeStudyDescriptionToText database upgrade failed: " + e.getMessage()
+				}
 			}
 		}
 	}
