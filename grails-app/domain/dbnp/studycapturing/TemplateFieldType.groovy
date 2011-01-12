74906dfec8ebd41adfcbec120a72c9a1fcbb16c8
keesvanbochove <kees@thehyve.nl>
2011-01-12 23:24:47 +0000

    changed templatefieldtype descriptions according to #268

--- grails-app/domain/dbnp/studycapturing/TemplateFieldType.groovy
+++ grails-app/domain/dbnp/studycapturing/TemplateFieldType.groovy
@@ -9,20 +9,20 @@ package dbnp.studycapturing
  * $Date$
  */
 public enum TemplateFieldType implements Serializable {
-	STRING		('String'				, 'Text'		, 'max 255 chars'),			// string
-	TEXT		('Long string'			, 'Text'		, 'unlimited'),				// text
+	STRING		('Short text'			, 'Text'		, 'max 255 chars'),			// string
+	TEXT		('Long text'			, 'Text'		, 'unlimited number of chars'),	// text
 	//INTEGER	('Integer number'		, 'Numerical'	, '1'),						// integer
 	//FLOAT		('Floating-point number', 'Numerical'	, '1.0'),					// float
-	DOUBLE		('Floating point number', 'Numerical'	, '1.0'),					// double
-	STRINGLIST	('List of items'		, 'Text'		, ''),						// string list
-	ONTOLOGYTERM('Ontology Reference'	, 'Other'		, ''),						// ontology reference
+	DOUBLE		('Decimal number', 'Numerical'	, '1.31'),					// double
+	STRINGLIST	('Dropdown selection of terms'		, 'Text'		, ''),						// string list
+	ONTOLOGYTERM('Term from ontology'	, 'Other'		, 'A term that comes from one or more selected ontologies'),						// ontology reference
 	DATE		('Date'					, 'Date'		, '2010-01-01'),			// date
 	RELTIME		('Relative time'		, 'Date'		, '3 days'),				// relative date, e.g. days since start of study
 	FILE		('File'					, 'Other'		, '')		,				// file
-	BOOLEAN		('Boolean'				, 'Other'		, 'true/false'),			// boolean
+	BOOLEAN		('True/false'			, 'Other'		, 'true/false'),			// boolean
 	TEMPLATE	('Template'				, 'Other'		, ''),						// template
-	MODULE		('Module'				, 'Other'		, ''),						// third party connected module,
-	LONG		('Integer number'		, 'Numerical'	, '100')					// long
+	MODULE		('Omics module'			, 'Other'		, ''),						// third party connected module,
+	LONG		('Natural number'		, 'Numerical'	, '100')					// long
     // TODO: add a timezone-aware date type to use for study start date
 
     String name
