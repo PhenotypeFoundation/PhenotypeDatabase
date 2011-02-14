1a9f1263f8956bcb82599d68f17f76a483baf77d
tjeerdnet <t.w.abma@gmail.com>
2011-02-14 21:48:41 +0000

    - initial Importer Wizard integration test with XML template file and NTC Study Excel file

---
+++ test/integration/gscf/ImporterTests.groovy
@@ -0,0 +1,69 @@
+package gscf
+
+import grails.test.*
+import org.springframework.core.io.*
+import grails.converters.*
+
+/**
+ * ImporterTests Test
+ *
+ * Description of my test
+ *
+ * @author  Tjeerd
+ * @since	20110314
+ * @package	gscf
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+class ImporterTests extends GroovyTestCase {
+    def ImporterService
+
+    static final String testStudyExcelFile = "testfiles/NTC_Experiment_test4.xls"
+    static final String testStudyXMLTemplateFile = "testfiles/ntc_tudy_template.xml"
+    static final String testSheetIndex = "0"
+    static final String testHeaderRow = "0"
+    static final String testDatamatrixStart = "0"
+    
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    /**
+    * Try to import an example Excel study and an XML study template
+    */
+    void testImportExcelAndXMLTemplateStudy() {
+        def xml
+        
+        // Load the files from a subfolder in the integration test folder and setup input streams
+        Resource resourceExcel = new ClassPathResource(testStudyExcelFile, getClass().classLoader)
+        Resource resourceXMLTemplate = new ClassPathResource(testStudyXMLTemplateFile, getClass().classLoader)
+
+        // Create input streams from the resources
+        def fisExcel = new FileInputStream(resourceExcel.getFile())
+        assert fisExcel
+
+        def fisXML = new FileInputStream(resourceXMLTemplate.getFile())
+        assert fisXML
+
+        // Retrieve the input stream as an Excel workbook
+        def wb = importerService.getWorkbook(fisExcel)
+        assert wb
+
+        // Parse XML
+		try {
+			xml = XML.parse(fisXML, "UTF-8")
+		} catch( Exception e ) {
+			println "Parsing failed" + e
+		}
+
+        assert xml
+        
+    }
+}
