0324839dbc13010bfbb96c7fa986c6aa612bf179
roberthorlings <robert@thehyve.nl>
2011-05-05 14:53:48 +0000

    Added 'returnUrl' parameter to /logout/remote to redirect the user after logging out. See #429

--- grails-app/controllers/dbnp/authentication/LogoutController.groovy
+++ grails-app/controllers/dbnp/authentication/LogoutController.groovy
@@ -26,11 +26,23 @@ class LogoutController {
 			authenticationService.logOffRemotely( params.consumer, params.token )
 		}
 		
+		def returnUrl;
+		
+		// If a returnUrl is given, use it for redirect
+		if( params.returnUrl ) {
+			returnUrl = params.returnUrl;
+		} else {
+			returnUrl = g.createLink(controller: 'home', absolute: true)
+		}
+		
+		println "REDIRECT: " + returnUrl;
+		println "parameters: " + params
+		
 		// Try to rest the redirect url
 		if( params[ SpringSecurityUtils.securityConfig.successHandler.targetUrlParameter ] ) {
-			redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl + "?" + SpringSecurityUtils.securityConfig.successHandler.targetUrlParameter + '=' + params[ SpringSecurityUtils.securityConfig.successHandler.targetUrlParameter ] // '/j_spring_security_logout'
+			redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl + "?spring-security-redirect=" + returnUrl?.encodeAsURL() + "&" + SpringSecurityUtils.securityConfig.successHandler.targetUrlParameter + '=' + params[ SpringSecurityUtils.securityConfig.successHandler.targetUrlParameter ] // '/j_spring_security_logout'
 		} else {
-			redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
+			redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl + "?spring-security-redirect=" + returnUrl?.encodeAsURL() // '/j_spring_security_logout'
 		}
 
 		// Remove all queries from session
