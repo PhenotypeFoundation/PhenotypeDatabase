29aad8918be5771f342837ad213705867154a353
tjeerdnet <t.w.abma@gmail.com>
2011-06-23 12:59:26 +0000

    - added comment

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -13,7 +13,13 @@
  * @date $Date$
 */
 
+
+
 eventCompileEnd = {
+   // Fix for addToWriters()-method failing, seems that when the default.properties containing the configured users
+   // cannot be read, the addToWriters()-method tries to add "null" users, which fails.
+   // Added this ant script which moves the default.properties to the correct classpath. Seems to work now.
+   // Might be a bug in Grails (at least till 1.3.7)?
 
    ant.echo (message: "Moving default configuration file (default.properties) ..." )
    ant.copy(file:"${basedir}/grails-app/conf/default.properties", todir:classesDirPath)
