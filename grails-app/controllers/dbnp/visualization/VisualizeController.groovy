8419c6a3f82cc1b14dd4f67d3f1244b6ca697298
roberthorlings <robert@thehyve.nl>
2011-08-25 08:57:25 +0000

    First attempt to create a visualization controller.

---
+++ grails-app/controllers/dbnp/visualization/VisualizeController.groovy
@@ -0,0 +1,24 @@
+/**
+ * Visualize Controller
+ *
+ * This controller enables the user to visualize his data
+ *
+ * @author  robert@thehyve.nl
+ * @since	20110825
+ * @package	dbnp.visualization
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+package dbnp.visualization
+
+class VisualizeController {
+	/**
+	 * Shows the visualization screen
+	 */
+    def index = { }
+	
+	
+}
---
+++ test/unit/dbnp/visualization/VisualizeControllerTests.groovy
@@ -0,0 +1,31 @@
+package dbnp.visualization
+
+import grails.test.*
+
+/**
+ * VisualizeControllerTests Test
+ *
+ * Description of my test
+ *
+ * @author  your email (+name?)
+ * @since	2010mmdd
+ * @package	???
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+class VisualizeControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
