6b5600e1cc1248545c8d8dc4f6c412878e26b089
4np <work@osx.eu>
2011-09-01 14:04:20 +0000

    - fixed restcontroller's getAssays method and removed temporary debug lines

--- grails-app/controllers/RestController.groovy
+++ grails-app/controllers/RestController.groovy
@@ -442,14 +442,18 @@ class RestController {
 			if (assay.module?.url) {
 				assayModuleURL = new URL(assay.module.url)
 				assayModuleInet = InetAddress.getByName(assayModuleURL.getHost())
-				println "assay module check: ${moduleInet.hostAddress == assayModuleInet.hostAddress}, ${moduleURL.path.replaceAll("/","") == assayModuleURL.path.replaceAll("/","")}, ${assay}"
-				println "modulePath: ${moduleURL.path}"
-				println "assayModulePath: ${assayModuleURL.path}"
 
+				// check if
+				// 	1. we've got an assay
+				// 	2. ip address of module matches
+				// 	3. the path part of the module matches
 				if (
-					moduleInet.hostAddress == assayModuleInet.hostAddress &&						// match ip addresses
-					moduleURL.path.replaceAll("/","") == assayModuleURL.path.replaceAll("/","") &&	// match host path
-					assay                                                       					// got assay?
+					assay &&
+					moduleInet.hostAddress == assayModuleInet.hostAddress &&
+					(
+						moduleURL.path.replaceAll(/[^a-zA-Z0-9]/,"") ==
+						assayModuleURL.path.replaceAll(/[^a-zA-Z0-9]/,"")
+					)
 				) {
 					def map = [assayToken : assay.giveUUID()]
 
