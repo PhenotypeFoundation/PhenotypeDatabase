ccb0df3cc878db18bd841851bc79fa9c9e44d499
4np <work@osx.eu>
2012-01-19 15:40:42 +0000

    initial study comparison controller

---
+++ grails-app/controllers/dbnp/query/StudyCompareController.groovy
@@ -0,0 +1,22 @@
+/**
+ * StudyCompareController Controler
+ *
+ * Description of my controller
+ *
+ * @author  your email (+name?)
+ * @since	2010mmdd
+ * @package	???
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+package dbnp.query
+
+class StudyCompareController {
+	/**
+	 * index closure
+	 */
+    def index = { }
+}
---
+++ test/unit/dbnp/query/StudyCompareControllerTests.groovy
@@ -0,0 +1,31 @@
+package dbnp.query
+
+import grails.test.*
+
+/**
+ * StudyCompareControllerTests Test
+ *
+ * Description of my test
+ *
+ * @author  your email (+name?)
+ * @since	2010mmdd
+ * @package	???
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+class StudyCompareControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
