d42e0cb0477d9a2267832ea0735771b2eea650cd
4np <work@osx.eu>
2012-02-06 12:11:04 +0000

    - reverted debug commit

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -56,9 +56,6 @@ log4j = {
 	//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
 	//}
 
-	info "grails.app"
-	debug "grails.app"
-
 	error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
 		'org.codehaus.groovy.grails.web.pages', //  GSP
 		'org.codehaus.groovy.grails.web.sitemesh', //  layouts
