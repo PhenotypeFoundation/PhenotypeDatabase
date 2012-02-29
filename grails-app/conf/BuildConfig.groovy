4285ec842cbc0918a12ef380b24ec9abe1afe537
4np <work@osx.eu>
2012-02-29 14:56:42 +0000

    - removing packaged libraries and using maven repositories instead

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,8 +29,13 @@ grails.project.dependency.resolution = {
         // from public Maven repositories
         //mavenLocal()
 		mavenCentral()
-		mavenRepo 'http://repository.jboss.org/maven2/'
 		mavenRepo "http://nexus.nmcdsp.org/content/repositories/releases"
+//		mavenRepo "http://snapshots.repository.codehaus.org"
+		mavenRepo "http://repository.codehaus.org"
+		mavenRepo "http://download.java.net/maven/2/"
+		mavenRepo "http://repository.jboss.org"
+		mavenRepo "http://maven.nuxeo.org/nexus/content/repositories/public"
+
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
@@ -39,7 +44,19 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // runtime 'mysql:mysql-connector-java:5.1.5'
-		build 'org.codehaus.gpars:gpars:0.11'
+//		build 'org.codehaus.gpars:gpars:0.11'
+		runtime 'postgresql:postgresql:9.1-901.jdbc3'
+		compile 'org.apache.poi:poi:3.7'
+		compile 'org.apache.poi:poi-ooxml:3.7'
+		compile 'org.apache.poi:poi-ooxml-schemas:3.7'
+//		compile 'xmlbeans:xbean:2.2.0'
+
+//		compile 'org.apache.poi:poi:3.7'
+//		compile 'org.apache.poi:poi-ooxml:3.7'
+//		compile 'org.apache.poi:poi-ooxml-schemas:3.7'
+//		http://repo1.maven.org/maven2/xmlbeans/xbean/2.2.0/xbean-2.2.0.jar
+//		http://repo1.maven.org/maven2/org/apache/poi/poi-ooxml/3.7/poi-ooxml-3.7.jar
+//		https://maven.nuxeo.org/nexus/content/repositories/public/org/jrobin/jrobin/1.5.9/jrobin-1.5.9.jar
     }
 }
 
