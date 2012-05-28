343f1890db7b584c40b2618dea1fa13ea088480f
keesvanbochove <kees@thehyve.nl>
2012-05-28 12:51:26 +0000

    declare plugin dependencies in BuildConfig instead of application.properties

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -58,6 +58,35 @@ grails.project.dependency.resolution = {
 //		http://repo1.maven.org/maven2/org/apache/poi/poi-ooxml/3.7/poi-ooxml-3.7.jar
 //		https://maven.nuxeo.org/nexus/content/repositories/public/org/jrobin/jrobin/1.5.9/jrobin-1.5.9.jar
     }
+	plugins {
+		compile(":hibernate:$grailsVersion")
+		compile ':tomcat:1.3.7.2'
+		compile ':jquery:1.7.1'
+		compile ':grom:0.2.3'
+		compile ':ajaxflow:0.2.1'
+		compile ':crypto:2.0'
+		compile ':spring-security-core:1.1.2'
+		compile(':gdt:0.2.1') {
+			// disable plugin dependency transition because it's horribly broken
+			// note: this assumes that ajaxflow, jquery and cryto stay included
+			transitive = false
+		}
+
+		compile(':gdtimporter:0.4.6.7') {
+		// see comment above on gdt, gdtimporter also requires spring security core (and shouldn't really be a plugin…)	
+		transitive = false
+		}
+		compile ':famfamfam:1.0.1'
+		compile ':jumpbar:0.1.5'
+		compile ':mail:1.0-SNAPSHOT'
+
+		compile ':trackr:0.6.4'
+		compile ':webflow:1.3.7'
+
+		runtime ':grails-melody:1.11'
+
+
+	}
 }
 
 //grails.plugin.location.'grom' = '../grom'
