f83d06634a1744289a092eb516fac540d8408bd7
Jeroen Wesbeek <work@osx.eu>
2012-06-28 14:29:18 +0200

    adding quart configuration

---
+++ grails-app/conf/QuartzConfig.groovy
@@ -0,0 +1,38 @@
+/**
+ * Quartz configuration
+ *
+ * @author  Jeroen Wesbeek <work@osx.eu>
+ * @since	20120628
+ * @package api
+ *
+ * Revision information:
+ * $Rev$
+ * $Author$
+ * $Date$
+ */
+
+quartz {
+    // auto start quartz scheduler on applicaiton bootstrap
+    autoStartup = true
+
+    // store jobs in database
+    jdbcStore = false
+
+    // wait for jobs to finish
+    waitForJobsToCompleteOnShutdown = true
+
+    // monitor jobs in melody
+    exposeSchedulerInRepository = true
+
+    props {
+        scheduler.skipUpdateCheck = true
+    }
+}
+
+environments {
+    test {
+        quartz {
+            autoStartup = false
+        }
+    }
+}
