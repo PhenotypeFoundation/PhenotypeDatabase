d292fdd2a74a1595148fdcdf2d03d744627172f1
Jeroen Wesbeek <work@osx.eu>
2012-07-31 16:36:21 +0200

    removed incompatible inherits

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,7 +20,6 @@ grails.project.dependency.resolution = {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-	    inherits true // Whether to inherit repository definitions from plugins
 	    grailsPlugins()
 	    grailsHome()
 	    grailsCentral()
