0234edd0272fdca1a1db24f5f22d69f4112bcb23
Jeroen Wesbeek <work@osx.eu>
2012-08-16 14:25:13 +0200

    Added support for shortcut URL's to studies
    e.g. studies.dbnp.org/study_code

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,19 @@
 class UrlMappings {
-    static mappings = {
-      "/$controller/$action?/$id?"{
-	      constraints {
-			 // apply constraints here
-		  }
-	  }
-      "/"(controller: 'home', action: 'index')
-	  "500"(view:'/error')
+	static mappings = {
+		// default mappings
+		"/$controller/$action?/$id?" {
+			constraints {
+				// apply constraints here
+			}
+		}
+
+		// landing page
+		"/"(controller: 'home', action: 'index')
+
+		// handle short codes
+		"/$shortCode"(controller: 'notFound', action: 'find')
+
+		// handle 500
+		"500"(view: '/error')
 	}
 }
