1ddd23215eca07e67feafdd0198c8fa46004f4e5
Ferry <FerryJagers@gmail.com>
2013-07-01 10:45:03 +0200

    Grails 2.2.3, dbxp-base & dbxp-sam version up

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -76,8 +76,8 @@ grails.project.dependency.resolution = {
 		)
 
 		compile(
-            ":dbxp-base:0.1.2.4",
-            ":dbxp-sam:0.9.5.7",
+            ":dbxp-base:0.1.2.5",
+            ":dbxp-sam:0.9.5.8",
 
             ":hibernate:$grailsVersion",
 
