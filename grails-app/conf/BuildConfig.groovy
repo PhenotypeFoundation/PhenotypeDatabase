6f8271c5a46fcd1892827a66ebbf2f9320de7e99
Seth Snel <sethsnel@gmail.com>
2013-09-06 12:44:52 +0200

    Release of version 0.9.1.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -76,7 +76,7 @@ grails.project.dependency.resolution = {
 		)
 
 		compile(
-            ":dbxp-base:0.1.2.9",
+            ":dbxp-base:0.1.3.0",
             ":dbxp-sam:0.9.6.1.rc",
 
             ":hibernate:$grailsVersion",
