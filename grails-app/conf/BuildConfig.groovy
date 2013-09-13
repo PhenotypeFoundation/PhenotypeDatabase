df50b0e0ef334026138638d4ccdcdd44c43ed797
Seth Snel <sethsnel@gmail.com>
2013-09-13 16:44:34 +0200

    Version up and update of plugin versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -76,8 +76,8 @@ grails.project.dependency.resolution = {
 		)
 
 		compile(
-            ":dbxp-base:0.1.3.0",
-            ":dbxp-sam:0.9.6.1.rc",
+            ":dbxp-base:0.1.3.1",
+            ":dbxp-sam:0.9.6.2",
 
             ":hibernate:$grailsVersion",
 
