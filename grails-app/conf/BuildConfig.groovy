cf84d2264d6c6cd7f5f59d8a2f9409883ca7afe0
Ferry <FerryJagers@gmail.com>
2013-11-22 17:23:57 +0100

    SAM version up

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -77,7 +77,7 @@ grails.project.dependency.resolution = {
 
 		compile(
             ":dbxp-base:0.1.3.1.rc1",
-            ":dbxp-sam:0.9.6.3.rc3",
+            ":dbxp-sam:0.9.6.3.rc4",
 
             ":hibernate:$grailsVersion",
 
