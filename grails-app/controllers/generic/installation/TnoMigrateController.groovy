7198a3e61147061ad2bf67703d14cfd65c1bb311
Seth Snel <sethsnel@gmail.com>
2013-12-04 16:57:19 +0100

    Update to TnoMigrateController

--- grails-app/controllers/generic/installation/TnoMigrateController.groovy
+++ grails-app/controllers/generic/installation/TnoMigrateController.groovy
@@ -15,112 +15,105 @@ import dbnp.studycapturing.SubjectEventGroup
  * Time: 12:10
  */
 class TnoMigrateController {
-    ArrayList<EventGroup> newEventGroups
 
     def index() {
     }
 
     def migrate() {
-        def message = "Good luck migrating";
+        def message = "Good luck migrating!";
 
         def studies = Study.findAll()
 
         for (Study study in studies) {
-            newEventGroups = new ArrayList<EventGroup>()
+            def oldEventGroups = [] + study.eventGroups
+            HashMap<String, EventGroup> newEventGroups = []
 
             if (study.id == 13478) {
-                for (Event event in study.events) {
-                    def MigrateEventGroups = event.getFieldValue("migration").split(";");
-                    for (String newEventGroup in MigrateEventGroups) {
-                        this.addEventGroup(newEventGroup, event)
-                    }
-                }
+                for(EventGroup eventGroup in oldEventGroups) {
+                    HashMap<String, ArrayList<Event>> temporaryEventGroups = []
 
-                for (SamplingEvent samplingEvent in study.samplingEvents) {
-                    def MigrateEventGroups = samplingEvent.getFieldValue("Migration").split(";");
-                    for (String newEventGroup in MigrateEventGroups) {
-                        this.addEventGroup(newEventGroup, samplingEvent)
+                    for (Event event in study.events) {
+                        def migrateEventGroups = event.getFieldValue("migration").split(";");
+                        for (String newEventGroup in migrateEventGroups) {
+                            this.addEventGroup(temporaryEventGroups, newEventGroup, event)
+                        }
                     }
-                }
 
-                for(EventGroup eventGroup in study.eventGroups) {
+                    for (SamplingEvent samplingEvent in study.samplingEvents) {
+                        def migrateEventGroups = samplingEvent.getFieldValue("Migration").split(";");
+                        for (String newEventGroup in migrateEventGroups) {
+                            this.addEventGroup(temporaryEventGroups, newEventGroup, samplingEvent)
+                        }
+                    }
+
                     for(Event event in eventGroup.eventInstances*.event.unique()) {
-                        def MigrateEventGroup = event.getFieldValue("migration");
-                        if(!MigrateEventGroup.contains(';')) {
-                            EventGroup newEventGroup = newEventGroups.find {it.name.equals(MigrateEventGroup)}
+                        def migrateEventGroup = event.getFieldValue("migration");
+                        if(!migrateEventGroup.contains(';')) {
+                            EventGroup newEventGroup = this.addNewEventGroup(study, newEventGroups, migrateEventGroup,
+                                    temporaryEventGroups.getAt(migrateEventGroup))
                             SubjectGroup subjectGroup = SubjectGroup.find {id == eventGroup.id}
-                            this.updateSubjectGroups(newEventGroup,subjectGroup)
-                        }
-                    }
-
-                    for(SamplingEvent samplingEvent in eventGroup.samplingEventInstances*.event.unique()) {
-                        def MigrateEventGroup = samplingEvent.getFieldValue("Migration");
-                        if(!MigrateEventGroup.contains(';')) {
-                            EventGroup newEventGroup = newEventGroups.find {it.name.equals(MigrateEventGroup)}
-                            SubjectGroup subjectGroup = SubjectGroup.find {id == eventGroup.id}
-                            this.updateSubjectGroups(newEventGroup,subjectGroup)
+                            this.updateSubjectGroups(study, newEventGroup,subjectGroup)
                         }
                     }
                 }
 
-                while(study.eventGroups.size() > 0) {
-                    study.deleteEventGroup(study.eventGroups.first())
+                for (eventGroup in oldEventGroups) {
+                    study.deleteEventGroup(eventGroup)
                 }
-                newEventGroups.each {
-                    study.addToEventGroups(it)
-                }
-                study.save(flush:true)
+                study.save(failOnError: true)
             }
         }
 
-
-        [message: message, newEventGroups: newEventGroups]
+        [message: message]
     }
 
-    private void addEventGroup(String name, event) {
-        EventGroup eventGroup = newEventGroups.find {it.name.equals(name)}
-        if (eventGroup != null) {
-            this.addEventToEventGroup(eventGroup,event)
+    private void addEventGroup(HashMap<String, ArrayList<Event>> newEventGroups, String name, event) {
+        if (newEventGroups.containsKey(name)) {
+            ArrayList<Event> eventListForEG = newEventGroups.get(name)
+            eventListForEG.push(event)
+            newEventGroups.putAt(name, eventListForEG)
         } else {
-            newEventGroups.add(this.newEventGroup(name,event))
+            newEventGroups.putAt(name, [event] as ArrayList)
         }
     }
 
-    private EventGroup newEventGroup(String name, Event event) {
-        def eventGroup = new EventGroup()
-        eventGroup.name = name
-        this.addEventToEventGroup(eventGroup,event)
-        eventGroup.save()
-        eventGroup
+    private EventGroup addNewEventGroup(Study study, HashMap<String, EventGroup> newEventGroups,
+                                        String name, ArrayList events) {
+        if(!newEventGroups.containsKey(name)) {
+            def eventGroup = new EventGroup()
+            eventGroup.name = name
+            study.addToEventGroups(eventGroup)
+            eventGroup.save(failOnError: true)
+            for (event in events) {
+                this.addEventToEventGroup(eventGroup,event)
+            }
+            newEventGroups.putAt(name,eventGroup)
+            eventGroup
+        } else {
+            newEventGroups.getAt(name)
+        }
     }
 
-    private EventGroup addEventToEventGroup(EventGroup eventGroup, event) {
+    private void addEventToEventGroup(EventGroup eventGroup, event) {
         def eventInEventGroup;
 
         if(event instanceof SamplingEvent) {
             eventInEventGroup = new SamplingEventInEventGroup()
-        } else {
-            eventInEventGroup = new EventInEventGroup()
-        }
-
-        eventInEventGroup.event = event
-        eventInEventGroup.eventGroup = eventGroup
-        eventInEventGroup.save()
-
-        if(event instanceof SamplingEvent) {
             eventGroup.addToSamplingEventInstances(eventInEventGroup)
         } else {
+            eventInEventGroup = new EventInEventGroup()
             eventGroup.addToEventInstances(eventInEventGroup)
         }
+        event.addToEventGroupInstances(eventInEventGroup)
 
-        eventGroup
+        eventInEventGroup.save(failOnError: true)
     }
 
-    private void updateSubjectGroups(EventGroup eventGroup, subjectGroup) {
+    private void updateSubjectGroups(Study study, EventGroup eventGroup, SubjectGroup subjectGroup) {
         SubjectEventGroup subjectEventGroup = new SubjectEventGroup()
-        subjectEventGroup.eventGroup = eventGroup
-        subjectEventGroup.subjectGroup = subjectGroup
-        subjectEventGroup.save()
         eventGroup.addToSubjectEventGroups(subjectEventGroup)
+        subjectGroup.addToSubjectEventGroups(subjectEventGroup)
+        study.addToSubjectEventGroups(subjectEventGroup)
+        subjectEventGroup.save(failOnError: true)
     }
 }
