55bbdad417df6b09c25798352ed1fb3839b50c3c
Seth Snel <sethsnel@gmail.com>
2013-12-06 15:27:54 +0100

    Yet another refactor to TnoMigrateController

--- grails-app/controllers/generic/installation/TnoMigrateController.groovy
+++ grails-app/controllers/generic/installation/TnoMigrateController.groovy
@@ -31,32 +31,44 @@ class TnoMigrateController {
             if (study.id == 13478) {
                 for(EventGroup eventGroup in oldEventGroups) {
                     HashMap<String, ArrayList<Event>> temporaryEventGroups = []
+                    def eventInstances = eventGroup.eventInstances + eventGroup.samplingEventInstances
 
-                    for (Event event in study.events) {
-                        def migrateEventGroups = event.getFieldValue("migration").split(";");
+                    // Collect unique set of eventgroups and their respective events
+                    eventInstances.each {
+                        def migrateEventGroups = it.event.getFieldValue("migration").split(";");
                         for (String newEventGroup in migrateEventGroups) {
-                            this.addEventGroup(temporaryEventGroups, newEventGroup, event)
+                            this.registerUniqueEventGroup(temporaryEventGroups, newEventGroup)
+                            this.addEventToEventGroupSet(temporaryEventGroups, it.event)
                         }
                     }
 
-                    for (SamplingEvent samplingEvent in study.samplingEvents) {
-                        def migrateEventGroups = samplingEvent.getFieldValue("Migration").split(";");
-                        for (String newEventGroup in migrateEventGroups) {
-                            this.addEventGroup(temporaryEventGroups, newEventGroup, samplingEvent)
-                        }
-                    }
+                    // Persist new eventgroups in database that:
+                    // - have multiple events
+                    // - or single event with only one eventgroup specified in migration column
+                    // Else ignore eventgroup
+                    temporaryEventGroups.each { eventGroupName, ArrayList eventSet ->
+                        if(!(eventSet.size() == 1 && eventSet.first().getFieldValue("Migration").split(";").size() > 1)) {
 
-                    for(Event event in eventGroup.eventInstances*.event.unique()) {
-                        def migrateEventGroup = event.getFieldValue("migration");
-                        if(!migrateEventGroup.contains(';')) {
-                            EventGroup newEventGroup = this.addNewEventGroup(study, newEventGroups, migrateEventGroup,
-                                    temporaryEventGroups.getAt(migrateEventGroup))
+                            // Collect minimum start to normalize starttimes
+                            def eventgroupEventInstances = eventInstances.findAll { eventSet.contains(it.event) }
+                            def minimumStartTime = eventgroupEventInstances.collect { it.startTime }.min()
+
+                            // Create and persist eventgroup
+                            EventGroup newEventGroup = this.createNewEventGroup(study, eventGroupName)
+
+                            // Associate events to the new eventgroup
+                            eventgroupEventInstances.each {
+                                this.addEventToEventGroup(it.event, newEventGroup, it.startTime - minimumStartTime)
+                            }
+
+                            // Associate subject groups with the new eventgroup
                             SubjectGroup subjectGroup = SubjectGroup.find {id == eventGroup.id}
-                            this.updateSubjectGroups(study, newEventGroup,subjectGroup)
+                            this.updateSubjectGroups(study, subjectGroup, newEventGroup, minimumStartTime)
                         }
                     }
                 }
 
+                //Remove old eventgroups
                 for (eventGroup in oldEventGroups) {
                     study.deleteEventGroup(eventGroup)
                 }
@@ -67,34 +79,27 @@ class TnoMigrateController {
         [message: message]
     }
 
-    private void addEventGroup(HashMap<String, ArrayList<Event>> newEventGroups, String name, event) {
-        if (newEventGroups.containsKey(name)) {
-            ArrayList<Event> eventListForEG = newEventGroups.get(name)
-            eventListForEG.push(event)
-            newEventGroups.putAt(name, eventListForEG)
-        } else {
-            newEventGroups.putAt(name, [event] as ArrayList)
+    private void registerUniqueEventGroup(HashMap<String, ArrayList<Event>> newEventGroups, String name) {
+        if (!newEventGroups.containsKey(name)) {
+            newEventGroups.putAt(name, [] as ArrayList)
         }
     }
 
-    private EventGroup addNewEventGroup(Study study, HashMap<String, EventGroup> newEventGroups,
-                                        String name, ArrayList events) {
-        if(!newEventGroups.containsKey(name)) {
+    private void addEventToEventGroupSet(HashMap<String, ArrayList<Event>> newEventGroups, event) {
+        ArrayList<Event> eventListForEG = newEventGroups.get(name)
+        eventListForEG.push(event)
+        newEventGroups.putAt(name, eventListForEG)
+    }
+
+    private EventGroup createNewEventGroup(Study study, String name) {
             def eventGroup = new EventGroup()
             eventGroup.name = name
             study.addToEventGroups(eventGroup)
             eventGroup.save(failOnError: true)
-            for (event in events) {
-                this.addEventToEventGroup(eventGroup,event)
-            }
-            newEventGroups.putAt(name,eventGroup)
             eventGroup
-        } else {
-            newEventGroups.getAt(name)
-        }
     }
 
-    private void addEventToEventGroup(EventGroup eventGroup, event) {
+    private void addEventToEventGroup(Event event, EventGroup eventGroup, startTime) {
         def eventInEventGroup;
 
         if(event instanceof SamplingEvent) {
@@ -104,13 +109,16 @@ class TnoMigrateController {
             eventInEventGroup = new EventInEventGroup()
             eventGroup.addToEventInstances(eventInEventGroup)
         }
-        event.addToEventGroupInstances(eventInEventGroup)
 
+        eventInEventGroup.startTime = startTime
+        event.addToEventGroupInstances(eventInEventGroup)
         eventInEventGroup.save(failOnError: true)
     }
 
-    private void updateSubjectGroups(Study study, EventGroup eventGroup, SubjectGroup subjectGroup) {
+    private void updateSubjectGroups(Study study, SubjectGroup subjectGroup, EventGroup eventGroup, startTime) {
         SubjectEventGroup subjectEventGroup = new SubjectEventGroup()
+        subjectEventGroup.startTime = startTime
+
         eventGroup.addToSubjectEventGroups(subjectEventGroup)
         subjectGroup.addToSubjectEventGroups(subjectEventGroup)
         study.addToSubjectEventGroups(subjectEventGroup)
