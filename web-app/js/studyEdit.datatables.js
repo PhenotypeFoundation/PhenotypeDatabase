PhenotypeFoundation/PhenotypeDatabase

commit 63ecd6b6d7439aae70a8c5e037039b38c7dad231

Changed the way file fields are visualized in study editor.

web-app/js/studyEdit.datatables.js
@@ -272,7 +272,7 @@ StudyEdit.datatables = {
 							
 							var fieldName = $( rowHeaders.get( idx ) ).data( "fieldname" );
 							StudyEdit.datatables.editable.cloning.cloneField( tableId, rowId, fieldName, $(td), aData[idx] );
-	    	
+
 						});
 				    }
 				});
@@ -292,9 +292,10 @@ StudyEdit.datatables = {
 				var fieldId = fieldPrefix + "." + rowId + "." + fieldName;
 				if( fieldType == "FILE" ) {
 					// Append the cloned input elements
-					$( td ).empty().append(
-						StudyEdit.datatables.editable.cloning.cloneFileField( prototypeId, fieldId, value, rowId, tableId )						
-					);				
+					$( td ).empty().html("<div class=\"fileWrapper\"></div>")
+                    $(td).find("div").html(
+                          StudyEdit.datatables.editable.cloning.cloneFileField( prototypeId, fieldId, value, rowId, tableId )
+                    );
 				} else {
 					// Append the cloned input elements
 					$( td ).empty().append(
@@ -351,12 +352,12 @@ StudyEdit.datatables = {
 				// Update the info field
 				field.filter( ".upload_info" )
 					.attr( "id", fieldId + "Example" )
-					.html( value ? "Current file: " + createFileHTML( value ) : "" );
+					.html( value ? "File: " + createFileHTML( value.substring(0,10) ) : "" );
 				
 				// UPdate the upload button
 				field.filter( ".upload_button" )
 					.attr( "id", "upload_button_" + fieldId );
-				
+
 				return field;
 			},
 			

web-app/css/studyEdit.css
@@ -47,8 +47,17 @@ div .studyEdit .description { width: 170px; }
 .studyEdit div#authorization .description { display: inline-block; *display: inline; width: 100px; margin-top: 6px; }
 
 /** START :: study page upload buttons **/
+.fileWrapper {
+    width: 140px;
+}
+
 div .upload_button {
-    display: inline; border: 1px solid #006dba; padding: 4px 8px; cursor: pointer; line-height: 26px;
+    display: inline;
+}
+
+div .upload_button img {
+    border-width: 0;
+    padding-left: 6px;
 }
 
 div .upload_info {

commit a6955983e1e711f5c487243b7f9e9cf461d612f1

Changes in the way file fields are shown.
This fixes a bug introduced in commit 63ecd6b6d7439aae70a8c5e037039b38c7dad231

web-app/js/studyEdit.datatables.js
@@ -355,7 +355,7 @@ StudyEdit.datatables = {
 				// Update the info field
 				field.filter( ".upload_info" )
 					.attr( "id", fieldId + "Example" )
-					.html( value ? "File: " + createFileHTML( value.substring(0,10) ) : "" );
+					.html( value ? "File: " + createFileHTML( value ) : "" );
 				
 				// UPdate the upload button
 				field.filter( ".upload_button" )

web-app/js/studyEdit.js
@@ -166,7 +166,11 @@ function deleteFile(field_id) {
 
 
 function createFileHTML(filename) {
-	return '<a target="_blank" href="' + baseUrl + '/file/get/' + filename + '">' + filename + '</a>';
+	var showFilename = filename;
+	if( showFilename.length > 10 )
+		showFilename = showFilename.substring(0,8) + "...";
+	
+	return '<a target="_blank" href="' + baseUrl + '/file/get/' + filename + '">' + showFilename + '</a>';
 }
 
 /*******************************************************************