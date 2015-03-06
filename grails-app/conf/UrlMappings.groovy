53d44200efa01f3c521b4884ec88f435951e49a4
Robert Horlings <robert@isdat.nl>
2015-03-06 11:59:23 +0100

    Updated UrlMapping to access SAM rest controller

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -24,19 +24,24 @@ class UrlMappings {
             }
         }
 		
+		// Accessing the SAM REST controller
+		"/measurements/$module/rest/$action?/$id?"(
+			controller: 'rest',
+			plugin: "dbxpSam"
+		)
+
 		// sam mapping for alternative url, including module name
-		"/module/$module/$controller/$action?/$id?" {
+		"/measurements/$module/$controller/$action?/$id?" {
 			constraints {
 				// apply constraints here
 			}
 		}
 
-
+		 // landing page for alternative SAM url
+		"/measurements/$module/assay/$action?/$id?"(controller: 'SAMAssay')
+		
         // landing page for alternative SAM url
         "/measurements/$module?"(controller: 'SAMHome', action: 'index')
-
-        // landing page for alternative SAM url
-        "/measurements/assay/$action?/$id?"(controller: 'SAMAssay')
 
 		// handle 404
 		"404"(controller: 'error', action: 'notFound')
