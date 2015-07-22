PhenotypeFoundation/PhenotypeDatabase

commit 906b3bc8f49261a47daa9fbac1ef9dfd0e893293

Refactored file upload javascripts to be easily reusable

web-app/js/studyEdit/studyEdit.datatables.js
@@ -480,7 +480,7 @@ StudyEdit.datatables = {
 			initializeFileFields: function( selector ) {
 				$(selector).find( ".fileField" ).each( function( idx, el ) {
 					var fieldId = $(el).attr( "id" );
-					fileUploadField( fieldId );
+					FileUpload.convertFileField( fieldId );
 					$(el).removeClass( "fileField" );
 				});
 			},

web-app/js/studyView/studyView.datatables.js
@@ -459,7 +459,7 @@ StudyView.datatables = {
 			initializeFileFields: function( selector ) {
 				$(selector).find( ".fileField" ).each( function( idx, el ) {
 					var fieldId = $(el).attr( "id" );
-					fileUploadField( fieldId );
+					FileUpload.convertFileField( fieldId );
 					$(el).removeClass( "fileField" );
 				});
 			},

grails-app/conf/GscfResources.groovy
@@ -85,7 +85,6 @@ modules = {
     gscfimporter { 
         dependsOn 'basicTabLayout',  'fileupload'
         
-        resource url: [dir: 'js', file: 'fileupload.js', plugin: 'gdt']
         resource url:[ dir:'js', file: 'importer.js']
         resource url:[ dir:'css', file: 'importer.css']
         resource url:[ dir:'css', file: 'templates.css']
@@ -179,6 +178,7 @@ modules = {
     
     'fileupload' {
         resource url: [dir: 'js', file: 'ajaxupload.3.6.js']
+        resource url: [dir: 'js', file: 'fileupload.js']
         resource url: [dir: 'css', file: 'fileupload.css']
     }
     
@@ -190,7 +190,6 @@ modules = {
         
         resource url: [dir: 'js', file: 'studywizard.js']
         resource url:[dir:'js', file: 'fuzzyStringMatch.js', plugin: 'gdt']
-        resource url: [dir: 'js', file: 'fileupload.js', plugin: 'gdt']
     }
     
     'add-more' {

local-plugins/GDT/grails-app/taglib/org/dbnp/gdt/GdtTagLib.groovy
@@ -476,15 +476,15 @@ class GdtTagLib extends AjaxflowTagLib {
         out << '<div id="upload_button_' + attrs.name + '" class="upload_button">'+buttonText+'</div>';
 
         if( !hideDelete )
-            out << '<a id="' + attrs.name + 'Delete" class="upload_del" href="#" onClick="if( confirm( \'Are you sure to delete this file?\' ) ) { deleteFile( \'' + attrs.name + '\' ); } return false;"><img src="' + resource(dir: 'images/icons', file: 'delete.png', plugin: 'famfamfam') + '"></a>';
+            out << '<a id="' + attrs.name + 'Delete" class="upload_del" href="#"><img src="' + resource(dir: 'images/icons', file: 'delete.png', plugin: 'famfamfam') + '"></a>';
 
         out << '<script type="text/javascript">';
         out << '  $(document).ready( function() { ';
         out << '    var filename = "' + ( attrs.value ?: "" ) + '";';
-        out << '    fileUploadField( "' + attrs.name + '" );';
+        out << '    FileUpload.convertFileField( "' + attrs.name + '", 20 );';
         out << '    if( filename != "" ) {';
         out << '      $("#' + attrs.name + 'Delete").show();';
-        out << '      $("#' + attrs.name + 'Example").html("File: " + createFileHTML( value.substring(0,10)))';
+        out << '      $("#' + attrs.name + 'Example").html("File: " + FileUpload.createFileHTML( filename, 20 ))';
         out << '    }';
         out << '  } );';
         out << "</script>\n";

local-plugins/GDT/web-app/js/fileupload.js
@@ -1,60 +1,0 @@
-/*************************************************
- *
- * Functions for file upload fields
- *
- ************************************************/
-
-// Create a file upload field
-function fileUploadField(field_id) {
-	/* example 2 */
-	new AjaxUpload('#upload_button_' + field_id, {
-		//action: 'upload.php',
-		action: baseUrl + '/file/upload/hoihoi', // I disabled uploads in this example for security reaaons
-		data : {},
-		name : field_id,
-		autoSubmit: true,
-		onChange : function(file, ext) {
-			oldFile = $('#' + field_id).val();
-			if (oldFile != '' && oldFile != 'existing*' && oldFile != '*deleted*' ) {
-				if (!confirm('The old file is deleted when uploading a new file. Do you want to continue?')) {
-					return false;
-				}
-			}
-
-			this.setData({
-				'field':   field_id,
-				'oldFile': oldFile
-			});
-
-			// Give feedback to the user
-			$('#' + field_id + 'Example').html('Uploading ' + createFileHTML(file));
-			$('#' + field_id + 'Delete').hide();
-
-		},
-		onComplete : function(file, response) {
-            //If there is HTML in the response, just retrieve the text value.
-            if(response.indexOf("<") != -1) {
-                response = $(response).text().split("//<![CDATA[")[0];
-            }
-			if (response == "") {
-				$('#' + field_id).val('');
-				$('#' + field_id + 'Example').html('<span class="error">Error uploading ' + createFileHTML(file) + '</span>');
-				$('#' + field_id + 'Delete').hide();
-			} else {
-				$('#' + field_id).val(response);
-				$('#' + field_id + 'Example').html('Uploaded ' + createFileHTML(file));
-				$('#' + field_id + 'Delete').show();
-			}
-		}
-	});
-}
-
-function deleteFile(field_id) {
-	$('#' + field_id).val('*deleted*');
-	$('#' + field_id + 'Example').html('File deleted');
-	$('#' + field_id + 'Delete').hide();
-}
-
-function createFileHTML(filename) {
-	return '<a target="_blank" href="' + baseUrl + '/file/get/' + filename + '">' + filename + '</a>';
-}

web-app/js/fileupload.js
@@ -1,0 +1,80 @@
+/*************************************************
+ *
+ * Functions for file upload fields
+ *
+ ************************************************/
+var FileUpload = {
+	// Create a file upload field
+	convertFileField: function(field_id, truncate) {
+		var that = this;
+		
+		// Convert the file field
+		new AjaxUpload('#upload_button_' + field_id, {
+			//action: 'upload.php',
+			action: baseUrl + '/file/upload/hoihoi', // I disabled uploads in this example for security reaaons
+			data : {},
+			name : field_id,
+			autoSubmit: true,
+			onChange : function(file, ext) {
+				oldFile = $('#' + field_id).val();
+				if (oldFile != '' && oldFile != 'existing*' && oldFile != '*deleted*' ) {
+					if (!confirm('The old file is deleted when uploading a new file. Do you want to continue?')) {
+						return false;
+					}
+				}
+
+				this.setData({
+					'field':   field_id,
+					'oldFile': oldFile
+				});
+
+				// Give feedback to the user
+				$('#' + field_id + 'Example').html('Uploading ' + that.createFileHTML(file, truncate));
+				$('#' + field_id + 'Delete').hide();
+
+			},
+			onComplete : function(file, response) {
+	            //If there is HTML in the response, just retrieve the text value.
+	            if(response.indexOf("<") != -1) {
+	                response = $(response).text().split("//<![CDATA[")[0];
+	            }
+				if (response == "") {
+					$('#' + field_id).val('');
+					$('#' + field_id + 'Example').html('<span class="error">Error uploading ' + that.createFileHTML(file, truncate) + '</span>');
+					$('#' + field_id + 'Delete').hide();
+				} else {
+					$('#' + field_id).val(response);
+					$('#' + field_id + 'Example').html('Uploaded ' + that.createFileHTML(file, truncate));
+					$('#' + field_id + 'Delete').show();
+				}
+			}
+		});
+		
+		// Enable delete button
+		$( '#' + field_id + "Delete").on( "click", function() {
+			if( confirm( 'Are you sure to delete this file?' ) ) { 
+				that.deleteFile( field_id ); 
+			} 
+			return false;
+		});
+
+	},
+	
+	deleteFile: function(field_id) {
+		$('#' + field_id).val('*deleted*');
+		$('#' + field_id + 'Example').html('File deleted');
+		$('#' + field_id + 'Delete').hide();
+	},
+
+	createFileHTML: function(filename, truncate) {
+		// See whether the filename should be truncated to a certain number of characters
+		var label = filename;
+		if( typeof(truncate) != 'undefined' && filename.length > truncate ) {
+			label = filename.substring(0, truncate) + "...";
+		}
+		
+		return '<a target="_blank" href="' + baseUrl + '/file/get/' + filename + '">' + label + '</a>';
+	}
+};
+
+

web-app/js/studyEdit/studyEdit.js
@@ -108,71 +108,6 @@ StudyEdit.initializePropertiesPage = function() {
     attachHelpTooltips();
 }
 
-/*************************************************
- *
- * Functions for file upload fields
- *
- ************************************************/
-// Create a file upload field
-function fileUploadField(field_id) {
-	/* example 2 */
-	new AjaxUpload('#upload_button_' + field_id, {
-		//action: 'upload.php',
-		action: baseUrl + '/file/upload', // I disabled uploads in this example for security reaaons
-		data : {},
-		name : field_id,
-		autoSubmit: true,
-		onChange : function(file, ext) {
-			oldFile = $('#' + field_id).val();
-			if (oldFile != '' && oldFile != 'existing*' && oldFile != '*deleted*' ) {
-				if (!confirm('The old file is deleted when uploading a new file. Do you want to continue?')) {
-					return false;
-				}
-			}
-
-			this.setData({
-				'field':   field_id,
-				'oldFile': oldFile
-			});
-
-			// Give feedback to the user
-			$('#' + field_id + 'Example').html('Uploading ' + createFileHTML(file));
-			$('#' + field_id + 'Delete').hide();
-
-		},
-		onComplete : function(file, response) {
-			if (response == "") {
-				$('#' + field_id).val('');
-				$('#' + field_id + 'Example').html('<span class="error">Error uploading ' + createFileHTML(file) + '</span>');
-				$('#' + field_id + 'Delete').hide();
-			} else {
-				// Sometimes, the response is returned with HTML tags. 
-				// It is unknown why this happens, but the tags are not needed.
-				response = response.replace(/<\/?[^>]+>/gi, '');
-
-				$('#' + field_id).val(response).trigger( "change" );
-				$('#' + field_id + 'Example').html('Uploaded ' + createFileHTML(file));
-				$('#' + field_id + 'Delete').show();
-			}
-		}
-	});
-}
-
-function deleteFile(field_id) {
-	$('#' + field_id).val('*deleted*');
-	$('#' + field_id + 'Example').html('File deleted');
-	$('#' + field_id + 'Delete').hide();
-}
-
-
-function createFileHTML(filename) {
-	var showFilename = filename;
-	if( showFilename.length > 10 )
-		showFilename = showFilename.substring(0,8) + "...";
-	
-	return '<a target="_blank" href="' + baseUrl + '/file/get/' + filename + '">' + showFilename + '</a>';
-}
-
 /*******************************************************************
  * 
  * Functions for date and timepickers 

web-app/js/studywizard.js
@@ -216,67 +216,6 @@ function showExampleReltime(inputfield) {
 
 /*************************************************
  *
- * Functions for file upload fields
- *
- ************************************************/
-
-// Create a file upload field
-function fileUploadField(field_id) {
-	/* example 2 */
-	new AjaxUpload('#upload_button_' + field_id, {
-		//action: 'upload.php',
-		action: baseUrl + '/file/upload', // I disabled uploads in this example for security reaaons
-		data : {},
-		name : field_id,
-		autoSubmit: true,
-		onChange : function(file, ext) {
-			oldFile = $('#' + field_id).val();
-			if (oldFile != '' && oldFile != 'existing*' && oldFile != '*deleted*' ) {
-				if (!confirm('The old file is deleted when uploading a new file. Do you want to continue?')) {
-					return false;
-				}
-			}
-
-			this.setData({
-				'field':   field_id,
-				'oldFile': oldFile
-			});
-
-			// Give feedback to the user
-			$('#' + field_id + 'Example').html('Uploading ' + createFileHTML(file));
-			$('#' + field_id + 'Delete').hide();
-
-		},
-		onComplete : function(file, response) {
-			if (response == "") {
-				$('#' + field_id).val('');
-				$('#' + field_id + 'Example').html('<span class="error">Error uploading ' + createFileHTML(file) + '</span>');
-				$('#' + field_id + 'Delete').hide();
-			} else {
-				// Sometimes, the response is returned with HTML tags. 
-				// It is unknown why this happens, but the tags are not needed.
-				response = response.replace(/<\/?[^>]+>/gi, '');
-
-				$('#' + field_id).val(response);
-				$('#' + field_id + 'Example').html('Uploaded ' + createFileHTML(file));
-				$('#' + field_id + 'Delete').show();
-			}
-		}
-	});
-}
-
-function deleteFile(field_id) {
-	$('#' + field_id).val('*deleted*');
-	$('#' + field_id + 'Example').html('File deleted');
-	$('#' + field_id + 'Delete').hide();
-}
-
-function createFileHTML(filename) {
-	return '<a target="_blank" href="' + baseUrl + '/file/get/' + filename + '">' + filename + '</a>';
-}
-
-/*************************************************
- *
  * Functions for adding publications to the study
  *
  ************************************************/

commit a5dc715ea8f037a5fd626ac47077b822f7f38f2a

Fix bug in rendering table entries with file values
introduced in 906b3bc8f49261a47daa9fbac1ef9dfd0e893293

web-app/js/studyEdit/studyEdit.datatables.js
@@ -361,7 +361,7 @@ StudyEdit.datatables = {
 				// Update the info field
 				field.filter( ".upload_info" )
 					.attr( "id", fieldId + "Example" )
-					.html( value ? "File: " + createFileHTML( value ) : "" );
+					.html( value ? "File: " + FileUpload.createFileHTML( value ) : "" );
 				
 				// UPdate the upload button
 				field.filter( ".upload_button" )
@@ -671,7 +671,7 @@ StudyEdit.datatables = {
 							otherTd.find( ".upload_info" ).html( "" );
 						} else {
 							otherTd.find( ".upload_del" ).show();
-							otherTd.find( ".upload_info" ).html( createFileHTML(value) );							
+							otherTd.find( ".upload_info" ).html( FileUpload.createFileHTML(value) );
 						}
 						
 						StudyEdit.datatables.editable.markChanged( field );

web-app/js/studyView/studyView.datatables.js
@@ -335,7 +335,7 @@ StudyView.datatables = {
 				// Update the info field
 				field.filter( ".upload_info" )
 					.attr( "id", fieldId + "Example" )
-					.html( value ? "File: " + createFileHTML( value ) : "" );
+					.html( value ? "File: " + FileUpload.createFileHTML( value ) : "" );
 				
 				// UPdate the upload button
 				field.filter( ".upload_button" )
@@ -646,7 +646,7 @@ StudyView.datatables = {
 							otherTd.find( ".upload_info" ).html( "" );
 						} else {
 							otherTd.find( ".upload_del" ).show();
-							otherTd.find( ".upload_info" ).html( createFileHTML(value) );							
+							otherTd.find( ".upload_info" ).html( FileUpload.createFileHTML(value) );
 						}
 						
 						StudyView.datatables.editable.markChanged( field );