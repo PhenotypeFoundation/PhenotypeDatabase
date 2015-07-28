c3606a8e64f4eff50ca2019d6885f1cf8d65f969
Robert Horlings <robert@isdat.nl>
2015-07-28 11:02:02 +0200

    FT-1544 Changed the name of eventgroup dialog

--- web-app/js/studyEdit/studyEdit.design.eventGroupDialog.js
+++ web-app/js/studyEdit/studyEdit.design.eventGroupDialog.js
@@ -174,7 +174,7 @@ StudyEdit.design.eventGroups = {
 	
 	edit: function( id, dataUrl ) {
 		var dialog = StudyEdit.design.eventGroups.dialog.get();
-		dialog.dialog( "option", "title", "Edit treatment & sample group" );
+		dialog.dialog( "option", "title", "Edit sample & treatment group" );
 		dialog.data( "eventgroup-id", id );
 		
 		StudyEdit.design.eventGroups.dialog.open()
