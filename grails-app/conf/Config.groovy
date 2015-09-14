PhenotypeFoundation/PhenotypeDatabase

commit 2c2d3f4e312528c10005c37b3d61f6cd8c90167e

FT-1601 Location of user guides is now configurable and can be put outside of the application

grails-app/conf/Config.groovy
@@ -260,3 +260,13 @@ fuzzyMatching.threshold = [
 ]
 
 gscf.baseURL = grails.serverURL
+
+// Default configuration of manuals
+gscf {
+    documents {
+        quickstart = "/downloads/quickstart_study_capturing.pdf"
+        manual = "/downloads/gscf_user_guide.pdf"
+        license = "/downloads/license_terms.pdf"
+        sam_userguide = "/downloads/SAM_User_Guide.pdf"
+    }
+}

grails-app/conf/default.properties
@@ -1,7 +1,6 @@
 ## Server URL ##
 grails.serverURL=http://localhost:8080/gscf
 gscf.baseURL=http://localhost:8080/gscf/
-gscf.issueURL=https://github.com/PhenotypeFoundation/GSCF/issues
 
 ## In-Memory Database ##
 dataSource.driverClassName=org.h2.Driver
@@ -53,3 +52,10 @@ uploads.uploadDir=/tmp
 
 # Change capitalization of sample names (default=true)
 gscf.reCapitalizeSampleNames=true
+
+# Locations of issues and documents
+gscf.issueURL=https://github.com/PhenotypeFoundation/GSCF/issues
+gscf.documents.quickstart=/gscf/downloads/quickstart_study_capturing.pdf
+gscf.documents.manual=/gscf/downloads/gscf_user_guide.pdf
+gscf.documents.license=/gscf/downloads/license_terms.pdf
+gscf.documents.sam_userguide=/gscf/downloads/SAM_User_Guide.pdf

grails-app/views/home/index.gsp
@@ -230,9 +230,9 @@
             <div class="document">
                 <h2>User Guide Downloads</h2>
                 <ul class="doclist">
-                    <li><a target="_blank" href="${resource(dir:'downloads', file: 'quickstart_study_capturing.pdf')}" title="">Quick Start User Guide</a></li>
-                    <li><a target="_blank" href="${resource(dir:'downloads', file: 'gscf_user_guide.pdf')}" title="">In Depth User Guide</a></li>
-                    <li><a target="_blank" href="${resource(dir:'downloads', file: 'license_terms.pdf')}" title="">License Terms</a></li>
+                    <li><a target="_blank" href="${grailsApplication.config.gscf.documents.quickstart}" title="">Quick Start User Guide</a></li>
+                    <li><a target="_blank" href="${grailsApplication.config.gscf.documents.manual}" title="">In Depth User Guide</a></li>
+                    <li><a target="_blank" href="${grailsApplication.config.gscf.documents.license}" title="">License Terms</a></li>
                 </ul>
             </div>
             <p class="note">If you encounter a problem or have a suggestion for improvement feel free to submit an issue <a href="${issueUrl}" title="">here</a></p>

local-plugins/SAM/grails-app/views/layouts/sammain.gsp
@@ -34,7 +34,7 @@
 		    		</ul>
 		    	</li>
                 <li>
-                    <a href="${resource(dir: 'guides', file: 'SAM_User_Guide.pdf', plugin: 'dbxp-sam')}" target="_blank">
+                    <a href="${grailsApplication.config.gscf.documents.sam_userguide}" target="_blank">
                         User Guide
                         <img src="${fam.icon(name:"page_white_acrobat")}" alt="(pdf)" style="vertical-align:text-bottom;"/>
                     </a>

commit 8be34d1d9fbb22a2f445539c60a31be5de2c0912

FT-1526 FT-1601 Fix user guide links after breakage in 2c2d3f4e312528c10005c37b3d61f6cd8c90167e

grails-app/conf/Config.groovy
@@ -266,9 +266,9 @@ gscf.baseURL = grails.serverURL
 // Default configuration of manuals
 gscf {
     documents {
-        quickstart = "/downloads/quickstart_study_capturing.pdf"
-        manual = "/downloads/gscf_user_guide.pdf"
-        license = "/downloads/license_terms.pdf"
-        sam_userguide = "/downloads/SAM_User_Guide.pdf"
+        quickstart = "downloads/quickstart_study_capturing.pdf"
+        manual = "downloads/gscf_user_guide.pdf"
+        license = "downloads/license_terms.pdf"
+        sam_userguide = "downloads/SAM_User_Guide.pdf"
     }
 }

grails-app/conf/default.properties
@@ -55,7 +55,7 @@ gscf.reCapitalizeSampleNames=true
 
 # Locations of issues and documents
 gscf.issueURL=https://github.com/PhenotypeFoundation/GSCF/issues
-gscf.documents.quickstart=/gscf/downloads/quickstart_study_capturing.pdf
-gscf.documents.manual=/gscf/downloads/gscf_user_guide.pdf
-gscf.documents.license=/gscf/downloads/license_terms.pdf
-gscf.documents.sam_userguide=/gscf/downloads/SAM_User_Guide.pdf
+gscf.documents.quickstart=downloads/quickstart_study_capturing.pdf
+gscf.documents.manual=downloads/gscf_user_guide.pdf
+gscf.documents.license=downloads/license_terms.pdf
+gscf.documents.sam_userguide=downloads/SAM_User_Guide.pdf

grails-app/views/home/index.gsp
@@ -230,9 +230,9 @@
             <div class="document">
                 <h2>User Guide Downloads</h2>
                 <ul class="doclist">
-                    <li><a target="_blank" href="${grailsApplication.config.gscf.documents.quickstart}" title="">Quick Start User Guide</a></li>
-                    <li><a target="_blank" href="${grailsApplication.config.gscf.documents.manual}" title="">In Depth User Guide</a></li>
-                    <li><a target="_blank" href="${grailsApplication.config.gscf.documents.license}" title="">License Terms</a></li>
+                    <li><a target="_blank" href="${grailsApplication.config.gscf.baseURL + grailsApplication.config.gscf.documents.quickstart}" title="">Quick Start User Guide</a></li>
+                    <li><a target="_blank" href="${grailsApplication.config.gscf.baseURL + grailsApplication.config.gscf.documents.manual}" title="">In Depth User Guide</a></li>
+                    <li><a target="_blank" href="${grailsApplication.config.gscf.baseURL + grailsApplication.config.gscf.documents.license}" title="">License Terms</a></li>
                 </ul>
             </div>
             <p class="note">If you encounter a problem or have a suggestion for improvement feel free to submit an issue <a href="${issueUrl}" title="">here</a></p>

local-plugins/SAM/grails-app/views/layouts/sammain.gsp
@@ -35,7 +35,7 @@
 		    		</ul>
 		    	</li>
                 <li>
-                    <a href="${grailsApplication.config.gscf.documents.sam_userguide}" target="_blank">
+                    <a href="${grailsApplication.config.gscf.baseURL + grailsApplication.config.gscf.documents.sam_userguide}" target="_blank">
                         User Guide
                         <img src="${fam.icon(name:"page_white_acrobat")}" alt="(pdf)" style="vertical-align:text-bottom;"/>
                     </a>