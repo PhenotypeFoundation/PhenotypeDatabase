ccfc766d9c96fca2d66a87f6f25a593131c22677
Peter Kok <peter@thehyve.nl>
2016-01-27 12:21:06 +0100

    FT-1622 Require template field name to be unique, even if type or unit are different

--- local-plugins/GDT/grails-app/controllers/org/dbnp/gdt/TemplateEditorController.groovy
+++ local-plugins/GDT/grails-app/controllers/org/dbnp/gdt/TemplateEditorController.groovy
@@ -426,10 +426,10 @@ class TemplateEditorController {
 
         // See whether this field already exists. It is checked by name, type and unit and entity
         // The search is done using search by example (see http://grails.org/DomainClass+Dynamic+Methods, method find)
-        def uniqueParams = [name: params.name, type: params.type, unit: params.unit, entity: params.entity];
+        def uniqueParams = [name: params.name, entity: params.entity];
         if (TemplateField.find(new org.dbnp.gdt.TemplateField(uniqueParams))) {
             response.status = 500;
-            render "A field with this name, type and unit already exists.";
+            render "A field with this name already exists.";
             return;
         }
 
