PhenotypeFoundation/PhenotypeDatabase

commit 813cd2e175ba8c0bb28d7bb90aa9a09ea9a3bb2b

Some small bugfixes

grails-app/conf/default.properties
@@ -57,10 +57,10 @@ gscf.reCapitalizeSampleNames=true
 
 ## Locations of issues and documents ##
 gscf.issueURL=https://github.com/PhenotypeFoundation/PhenotypeDatabase/issues
-gscf.documents.quickguide=/interventionstudies/downloads/phenotype_database_quick_guide.pdf
-gscf.documents.study_manual=/interventionstudies/downloads/phenotype_database_study_manual.pdf
-gscf.documents.module_manual=/interventionstudies/downloads/phenotype_database_module_manual.pdf
-gscf.documents.license=/interventionstudies/downloads/phenotype_database_license_terms.pdf
+gscf.documents.quickguide=/gscf/downloads/phenotype_database_quick_guide.pdf
+gscf.documents.study_manual=/gscf/downloads/phenotype_database_study_manual.pdf
+gscf.documents.module_manual=/gscf/downloads/phenotype_database_module_manual.pdf
+gscf.documents.license=/gscf/downloads/phenotype_database_license_terms.pdf
 
 ## Email configuration ##
 grails.mail.host=smtp.gmail.com

local-plugins/SAM/grails-app/views/layouts/sammain.gsp
@@ -9,8 +9,6 @@
 	        <g:layoutHead />
 			
 			<link rel="shortcut icon" href="${resource(dir: 'images', file: 'favicon.ico')}" type="image/x-icon"/>
-	        
-	        <script type="text/javascript">baseUrl = '${grailsApplication.config.grails.serverURL}';</script>
 	    </head>
 	    <body>
 			<content tag="topnav">

grails-app/views/home/index.gsp
@@ -72,7 +72,7 @@
 							{ name: 'Published public', y: ${publishedPublicStudyCount}, color: '#45bee1' },
 							{ name: 'Unpublished Public', y: ${unPublishedPublicStudyCount}, color: '#e9644f' },
 							{ name: 'Published Private', y: ${publishedPrivateStudyCount}, color: '#2087a3' },
-							{ name: 'Unpublished Private', y: ${unPublishedPrivateStudyCount}, color: '#2087a3' }
+							{ name: 'Unpublished Private', y: ${unPublishedPrivateStudyCount}, color: '#e9644f' }
 						],
 						dataLabels: {
 							enabled: false

local-plugins/SAM/grails-app/controllers/org/dbxp/sam/PlatformController.groovy
@@ -162,7 +162,7 @@ class PlatformController {
 
         // Attempt to save platform
         if (!platformInstance.save(flush: true)) {
-            render(view: "create", model: [platformInstance: platformInstance], module: params.module)
+            render(view: "create", model: [platformInstance: platformInstance, module: params.module])
             return
         }
 
@@ -246,7 +246,7 @@ class PlatformController {
         platformInstance.properties = params
 
         if (!platformInstance.save(flush: true)) {
-            render(view: "edit", model: [platformInstance: platformInstance], module: params.module)
+            render(view: "edit", model: [platformInstance: platformInstance, module: params.module])
             return
         }
 
@@ -275,7 +275,7 @@ class PlatformController {
 	
 	// Get a list of template specific fields
 	def templateSelection = {
-		render(template: "templateSelection", model: [template: _determineTemplate()], module: params.module)
+		render(template: "templateSelection", model: [template: _determineTemplate(), module: params.module])
 	}
 
     def returnUpdatedTemplateSpecificFields = {
@@ -293,7 +293,7 @@ class PlatformController {
             log.error( e );
         }
 
-        render(template: "templateSpecific", model: [template: template, values: values], module: params.module)
+        render(template: "templateSpecific", model: [template: template, values: values, module: params.module])
     }
 
     /**

local-plugins/SAM/src/groovy/org/dbxp/sam/importer/SampleLayoutMeasurementsImporter.groovy
@@ -67,12 +67,7 @@ public class SampleLayoutMeasurementsImporter extends AbstractImporter {
         // The user could choose from the features belonging to the selected platform
         // The user could also choose a column to contain the sample name
         def options = [
-                [ id: 'subject', name: "Subject name" ],
-                [ id: 'subject', name: "subject name" ],
-                [ id: 'subject', name: "Subjectname" ],
-                [ id: 'subject', name: "subjectname" ],
-                [ id: 'subject', name: "Name" ],
-                [ id: 'subject', name: "name" ]
+            [ id: 'sample', name: "Sample name" ]
         ]
         
         // Create a list of domain fields and template fields to match against

local-plugins/SAM/src/groovy/org/dbxp/sam/importer/SubjectLayoutMeasurementsImporter.groovy
@@ -67,11 +67,7 @@ public class SubjectLayoutMeasurementsImporter extends AbstractImporter {
         // The user could choose from the features belonging to the selected platform
         // The user could also choose a column to contain the sample name
         def options = [
-            [ id: 'subject', name: "Subject name" ],
-            [ id: 'subject', name: "Subjectname" ],
-            [ id: 'subject', name: "subjectname" ],
-            [ id: 'subject', name: "Name" ],
-            [ id: 'subject', name: "name" ]
+            [ id: 'subject', name: "Subject name" ]
         ]
         
         // Create a list of domain fields and template fields to match against

commit 879be886c6ddcfcb4a1c8eb85439d9b788c1a93d

Fixed bug introduced in 813cd2e175ba8c0bb28d7bb90aa9a09ea9a3bb2

grails-app/conf/default.properties
@@ -1,7 +1,5 @@
 ## Server URL ##
 grails.serverURL=http://localhost:8080/gscf
-## baseURL used by (dedicated) modules ##
-gscf.baseURL=http://localhost:8080/gscf
 
 ## In-Memory Database ##
 dataSource.driverClassName=org.h2.Driver

local-plugins/SAM/grails-app/views/layouts/sammain.gsp
@@ -7,8 +7,10 @@
 	        <r:require modules="sam2"/>
 
 	        <g:layoutHead />
-			
+
 			<link rel="shortcut icon" href="${resource(dir: 'images', file: 'favicon.ico')}" type="image/x-icon"/>
+
+			<script type="text/javascript">baseUrl = '${grailsApplication.config.grails.serverURL}';</script>
 	    </head>
 	    <body>
 			<content tag="topnav">
@@ -38,7 +40,7 @@
                         <img src="${fam.icon(name:"page_white_acrobat")}" alt="(pdf)" style="vertical-align:text-bottom;"/>
                     </a>
                 </li>
-				<li><g:link url="${grailsApplication.config.gscf.baseURL}">Back to design</g:link></li>
+				<li><g:link url="${grailsApplication.config.grails.serverURL}">Back to design</g:link></li>
                 <g:if test="${grailsApplication.config.module.showVersionInfo}">
                     <li style="font-size: 9px; color: #888;"><g:message code="meta.app.version" default="Version: {0}" args="[meta(name: 'app.version')]"/><br />Changeset: <g:render template="/version"/></li>
                 </g:if>